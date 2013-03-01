78031ca1077a224d37c4f549cd8dac55edd4ed5f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-03-01 03:22:13 +0100

    Add custom NRW redirect url

--- fragdenstaat_de/urls.py
+++ fragdenstaat_de/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls import patterns, url
+from django.http import HttpResponseRedirect
 
 
 urlpatterns = patterns('fragdenstaat_de.views',
     (r'^presse/(?P<slug>[-\w]+)/$', 'show_press', {}, 'fds-show_press'),
+    url(r'^nordrhein-westfalen/', lambda request: HttpResponseRedirect('/nrw/'),
+        name="jurisdiction-nrw-redirect")
 )
