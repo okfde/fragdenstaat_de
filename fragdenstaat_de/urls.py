4280fb51890c4680c72cbc9b2dcff9179604cf63
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-12-02 13:56:15 +0100

    Add foiidea URL include

--- fragdenstaat_de/urls.py
+++ fragdenstaat_de/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.http import HttpResponseRedirect
 
 
@@ -6,4 +6,9 @@ urlpatterns = patterns('fragdenstaat_de.views',
     (r'^presse/(?P<slug>[-\w]+)/$', 'show_press', {}, 'fds-show_press'),
     url(r'^nordrhein-westfalen/', lambda request: HttpResponseRedirect('/nrw/'),
         name="jurisdiction-nrw-redirect")
+)
+
+urlpatterns += patterns('',
+    # Translators: URL part
+    (r'^nachrichten/', include('foiidea.urls')),
 )
