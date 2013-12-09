f727b6663a522577e7c51878de660f5d7306e9ba
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-12-09 15:51:11 +0100

    Add custom Rheinland-Pfalz redirect

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -5,7 +5,9 @@ from django.http import HttpResponseRedirect
 urlpatterns = patterns('fragdenstaat_de.theme.views',
     (r'^presse/(?P<slug>[-\w]+)/$', 'show_press', {}, 'fds-show_press'),
     url(r'^nordrhein-westfalen/', lambda request: HttpResponseRedirect('/nrw/'),
-        name="jurisdiction-nrw-redirect")
+        name="jurisdiction-nrw-redirect"),
+    url(r'^rlp/', lambda request: HttpResponseRedirect('/rheinland-pfalz/'),
+        name="jurisdiction-rlp-redirect"),
 )
 
 urlpatterns += patterns('',
