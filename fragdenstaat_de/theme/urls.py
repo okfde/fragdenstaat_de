5f115d6ec2a6e2524882d4741fc7b8766846c3d4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-12-27 13:36:48 +0100

    Add index page for press area

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 
 
 urlpatterns = patterns('fragdenstaat_de.theme.views',
+    (r'^presse/$', 'show_press', {}, 'fds-show_press'),
     (r'^presse/(?P<slug>[-\w]+)/$', 'show_press', {}, 'fds-show_press'),
     url(r'^nordrhein-westfalen/', lambda request: HttpResponseRedirect('/nrw/'),
         name="jurisdiction-nrw-redirect"),
--- fragdenstaat_de/theme/views.py
+++ fragdenstaat_de/theme/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, Http404
 from django.template import TemplateDoesNotExist
 
 
-def show_press(request, slug):
+def show_press(request, slug=None):
+    if slug is None:
+        return render(request, 'press/index.html')
     try:
         return render(request, 'press/%s.html' % slug)
     except TemplateDoesNotExist:
