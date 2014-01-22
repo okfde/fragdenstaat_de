6ccd9177c4f443d852c46e97f8e01c8c239d4a71
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-01-22 09:45:22 +0100

    Add Zensurheberrecht campaign

--- fragdenstaat_de/theme/views.py
+++ fragdenstaat_de/theme/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, Http404
 from django.template import TemplateDoesNotExist
 
+from froide.helper.cache import cache_anonymous_page
 
+
+@cache_anonymous_page(15 * 60)
 def show_press(request, slug=None):
     if slug is None:
         return render(request, 'press/index.html')
