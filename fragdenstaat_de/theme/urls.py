8d6d609cbc4d2059409f90d0f7f563c2c4030425
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-05-21 17:23:28 +0200

    Move press listings into flatpages

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include
 
 
-urlpatterns = patterns('fragdenstaat_de.theme.views',
-    (r'^presse/$', 'show_press', {}, 'fds-show_press'),
-    (r'^presse/(?P<slug>[-\w]+)/$', 'show_press', {}, 'fds-show_press'),
-)
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     # Translators: URL part
     (r'^nachrichten/', include('foiidea.urls')),
 )
