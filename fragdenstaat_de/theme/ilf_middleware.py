8bd02d7e7d854b9f7af304acb747ed9b48514760
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-06-19 19:44:44 +0200

    Downgrade to older 1.6 middleware implementation

--- fragdenstaat_de/theme/ilf_middleware.py
+++ fragdenstaat_de/theme/ilf_middleware.py
@@ -2,10 +2,10 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.utils.crypto import constant_time_compare
-from django.utils.encoding import force_text
 from django.utils.http import same_origin
 from django.middleware.csrf import (CsrfViewMiddleware, _sanitize_token,
-    REASON_BAD_REFERER, REASON_NO_CSRF_COOKIE, REASON_BAD_TOKEN)
+                                    _get_new_csrf_key, REASON_BAD_REFERER,
+                                    REASON_NO_CSRF_COOKIE, REASON_BAD_TOKEN)
 
 
 class CsrfViewIlfMiddleware(CsrfViewMiddleware):
@@ -25,6 +25,9 @@ class CsrfViewIlfMiddleware(CsrfViewMiddleware):
             request.META['CSRF_COOKIE'] = csrf_token
         except KeyError:
             csrf_token = None
+            # Generate token and store it in the request, so it's
+            # available to the view.
+            request.META["CSRF_COOKIE"] = _get_new_csrf_key()
 
         # Wait until request.META["CSRF_COOKIE"] has been manipulated before
         # bailing out, so that get_token still works
@@ -57,23 +60,14 @@ class CsrfViewIlfMiddleware(CsrfViewMiddleware):
                 # Barth et al. found that the Referer header is missing for
                 # same-domain requests in only about 0.2% of cases or less, so
                 # we can use strict Referer checking.
-                referer = force_text(
-                    request.META.get('HTTP_REFERER'),
-                    strings_only=True,
-                    errors='replace'
-                )
-                #######
-                # Change start
-                #######
+                referer = request.META.get('HTTP_REFERER')
                 if referer is not None:
                     # Note that request.get_host() includes the port.
                     good_referer = 'https://%s/' % request.get_host()
                     if not same_origin(referer, good_referer):
                         reason = REASON_BAD_REFERER % (referer, good_referer)
                         return self._reject(request, reason)
-            #######
-            # Change end
-            #######
+
             if csrf_token is None:
                 # No CSRF cookie. For POST requests, we insist on a CSRF cookie,
                 # and in this way we can avoid all CSRF attacks, including login
@@ -83,20 +77,12 @@ class CsrfViewIlfMiddleware(CsrfViewMiddleware):
             # Check non-cookie token for match.
             request_csrf_token = ""
             if request.method == "POST":
-                try:
-                    request_csrf_token = request.POST.get('csrfmiddlewaretoken', '')
-                except IOError:
-                    # Handle a broken connection before we've completed reading
-                    # the POST data. process_view shouldn't raise any
-                    # exceptions, so we'll ignore and serve the user a 403
-                    # (assuming they're still listening, which they probably
-                    # aren't because of the error).
-                    pass
+                request_csrf_token = request.POST.get('csrfmiddlewaretoken', '')
 
             if request_csrf_token == "":
                 # Fall back to X-CSRFToken, to make things easier for AJAX,
                 # and possible for PUT/DELETE.
-                request_csrf_token = request.META.get(settings.CSRF_HEADER_NAME, '')
+                request_csrf_token = request.META.get('HTTP_X_CSRFTOKEN', '')
 
             if not constant_time_compare(request_csrf_token, csrf_token):
                 return self._reject(request, REASON_BAD_TOKEN)
