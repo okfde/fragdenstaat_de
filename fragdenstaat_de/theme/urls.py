f70122d230e3705420cc8e71b8a0a32c080543d1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-01-23 16:50:54 +0100

    Use default views

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib.flatpages import views
 
-from .views import index
+from froide.foirequest.views import index
 
 urlpatterns = [
     url(r'^hilfe/spenden/$', views.flatpage, {'url': '/hilfe/spenden/'}, name='help-donate'),
