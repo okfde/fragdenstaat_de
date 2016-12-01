18878f90066161cd6ee6e8f74a068a6aa2736493
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-12-01 17:42:58 +0100

    Move legalaction url to 'klagen'

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -6,6 +6,6 @@ from .views import index
 urlpatterns = [
     url(r'^hilfe/spenden/$', views.flatpage, {'url': '/hilfe/spenden/'}, name='help-donate'),
     url(r'^kampagne/', include('froide_campaign.urls')),
-    url(r'^verklagen/', include('froide_legalaction.urls')),
+    url(r'^klagen/', include('froide_legalaction.urls')),
     url(r'^$', index, name='index'),
 ]
