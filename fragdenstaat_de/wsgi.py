59e48e5cb0a9c11e086e4cb77e593cc8917c61fa
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-04-17 19:22:24 +0200

    Add wsgi file

---
+++ fragdenstaat_de/wsgi.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# Calculate the path based on the location of the WSGI script.
+current_dir = os.path.dirname(__file__)
+project = os.path.dirname(current_dir)
+
+sys.path.append(project)
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fragdenstaat_de.settings.development')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
+
+from django.conf import settings
+from configurations.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
+try:
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
+
+    if settings.MEDIA_ROOT and settings.MEDIA_URL:
+        application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
+except ImportError:
+    pass
