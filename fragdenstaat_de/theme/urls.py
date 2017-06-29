72e90efee9f306a3d190d42b7cbef090f5f8c39e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-06-29 10:13:52 +0200

    Add gesetze campaign dashboard

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib.flatpages import views
 
-from .views import index
+from .views import index, gesetze_dashboard
 
 urlpatterns = [
     url(r'^hilfe/spenden/$', views.flatpage, {'url': '/hilfe/spenden/'}, name='help-donate'),
     url(r'^kampagne/', include('froide_campaign.urls')),
     url(r'^klagen/', include('froide_legalaction.urls')),
+    url(r'^gesetze/dashboard/$', gesetze_dashboard, name='fragdenstaat-gesetze_dashboard'),
     url(r'^$', index, name='index'),
 ]
--- fragdenstaat_de/theme/views.py
+++ fragdenstaat_de/theme/views.py
@@ -5,6 +5,8 @@ from froide.publicbody.models import PublicBody
 from froide.frontpage.models import FeaturedRequest
 from froide.helper.cache import cache_anonymous_page
 
+from froide_campaign.models import InformationObject
+
 
 @cache_anonymous_page(15 * 60)
 def index(request):
@@ -18,4 +20,55 @@ def index(request):
         'successful_foi_requests': successful_foi_requests,
         'foicount': FoiRequest.published.for_list_view().count(),
         'pbcount': PublicBody.objects.get_list().count()
+    })
+
+
+@cache_anonymous_page(30 * 60)
+def gesetze_dashboard(request):
+    PUBLISHES = {
+        'Bundesministerium der Justiz und für Verbraucherschutz': ('ja, seit 2016', 'success'),
+    }
+
+    NUMBER_OF_TOP_ORGS = {
+        'Auswärtiges Amt': (9, 1),
+        'Beauftragte der Bundesregierung für Kultur und Medien': (2, 2),
+        'Bundesministerium für Bildung und Forschung': (16, 1),
+        'Bundesministerium für Umwelt, Naturschutz, Bau und Reaktorsicherheit': (2, 22),
+        'Bundesministerium für Verkehr und digitale Infrastruktur': (2, 13),
+        'Bundesministerium für Wirtschaft und Energie': (2, 13),
+    }
+
+    TOP_VERBAND = {
+        'Bundesministerium der Finanzen': ('Ifo-Institut für Wirtschaftsforschung', 26),
+        'Bundesministerium der Justiz und für Verbraucherschutz': ('Deutscher Anwaltverein e. V. (DAV)', 51),
+        'Bundesministerium des Innern': ('Evangelische Kirche in Deutschland - EKD', 16),
+        'Bundesministerium für Arbeit und Soziales': ('Bundesvereinigung der Deutschen Arbeitgeberverbände', 11),
+        'Bundesministerium für Ernährung und Landwirtschaft': ('Deutscher Bauernverband e. V. (DBV)', 13),
+        'Bundesministerium für Familie, Senioren, Frauen und Jugend': ('Deutscher Städtetag', 4),
+        'Bundesministerium für Gesundheit': ('GKV-Spitzenverband', 10),
+    }
+
+    from django.db import models
+
+    pbs = PublicBody.objects.filter(
+        informationobject__campaign__slug='stellungnahmen-zu-gesetzentwurfen'
+        ).annotate(
+            count=models.Count('informationobject'),
+            foirequest_count=models.Count('informationobject__foirequest')
+        ).distinct()
+
+    for pb in pbs:
+        pb.publishes_laws = (('nein', 'danger') if pb.name not in PUBLISHES
+                             else PUBLISHES[pb.name])
+        pb.top_verband = (None if pb.name not in TOP_VERBAND
+                             else TOP_VERBAND[pb.name])
+        pb.top_count = (None if pb.name not in NUMBER_OF_TOP_ORGS
+                             else NUMBER_OF_TOP_ORGS[pb.name])
+        pb.request_made_percent = round(pb.foirequest_count / pb.count * 100)
+
+    embed = request.GET.get('embed', None)
+
+    return render(request, 'campaign/gesetze_dashboard.html', {
+        'base': 'simple_base.html' if embed else 'base.html',
+        'pbs': pbs
     })
