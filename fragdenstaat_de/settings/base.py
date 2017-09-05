bb755fda23bd8199e52db4a1e970d3b5b124137f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-09-05 17:48:09 +0200

    Use raw strings + unicode_literals future import

--- fragdenstaat_de/settings/base.py
+++ fragdenstaat_de/settings/base.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import os
 import re
 
@@ -133,11 +134,11 @@ class FragDenStaatBase(German, ThemeBase, Base):
                 (30, 7 * 24 * 60 * 60),  # 15 requests in 7 days
             ],
             greetings=[
-                rec(u"Sehr geehrt(er? (?:Herr|Frau)(?: ?Dr\.?)?(?: ?Prof\.?)? .*)"),
+                rec(r"Sehr geehrt(er? (?:Herr|Frau)(?: ?Dr\.?)?(?: ?Prof\.?)? .*)"),
             ],
             custom_replacements=[
                 rec(r'[Bb][Gg]-[Nn][Rr]\.?\s*\:?\s*([a-zA-Z0-9\s/]+)')
             ],
-            closings=[rec(u"([Mm]it )?(den )?(freundliche(n|m)?|vielen|besten)? ?Gr(ü|u)(ß|ss)(en?)?,?"), rec("Hochachtungsvoll,?"), rec('i\. ?A\.'), rec('[iI]m Auftrag')]
+            closings=[rec(r"([Mm]it )?(den )?(freundliche(n|m)?|vielen|besten)? ?Gr(ü|u)(ß|ss)(en?)?,?"), rec("Hochachtungsvoll,?"), rec(r'i\. ?A\.'), rec('[iI]m Auftrag')]
         ))
         return config
