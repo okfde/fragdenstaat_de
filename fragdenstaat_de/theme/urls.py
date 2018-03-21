e1453084101537d8e4bbcbf21a6385d28c6f9a5f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-03-21 15:55:21 +0100

    Add Sitemap support for CMS and blog

--- fragdenstaat_de/theme/urls.py
+++ fragdenstaat_de/theme/urls.py
@@ -1,9 +1,34 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib.flatpages import views
+from django.contrib.sitemaps import views as sitemaps_views
 
-from froide.urls import froide_urlpatterns, help_urlpatterns, jurisdiction_urls
+from froide.urls import (
+    froide_urlpatterns, help_urlpatterns,
+    jurisdiction_urls, sitemaps
+)
+
+from cms.sitemaps import CMSSitemap
+from djangocms_blog.sitemaps import BlogSitemap
 
 from .views import index, gesetze_dashboard
+
+
+sitemaps['cmspages'] = CMSSitemap
+sitemaps['blog'] = BlogSitemap
+
+PROTOCOL = settings.SITE_URL.split(':')[0]
+
+for klass in sitemaps.values():
+    klass.protocol = PROTOCOL
+
+
+sitemap_urlpatterns = [
+    url(r'^sitemap\.xml$', sitemaps_views.index,
+        {'sitemaps': sitemaps, 'sitemap_url_name': 'sitemaps'}),
+    url(r'^sitemap-(?P<section>.+)\.xml$', sitemaps_views.sitemap,
+        {'sitemaps': sitemaps}, name='sitemaps')
+]
 
 urlpatterns = [
     url(r'^hilfe/spenden/$', views.flatpage, {'url': '/hilfe/spenden/'}, name='help-donate'),
@@ -18,7 +43,12 @@ urlpatterns += [
     url(r'^', include('filer.server.urls')),
 ]
 
-urlpatterns += froide_urlpatterns + help_urlpatterns + jurisdiction_urls
+urlpatterns += (
+    sitemap_urlpatterns +
+    froide_urlpatterns +
+    help_urlpatterns +
+    jurisdiction_urls
+)
 
 urlpatterns += [
     url(r'^', include('cms.urls')),
