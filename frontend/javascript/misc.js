d5378bcc91bcbc65528797dba8d8eefca7582401
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-07-18 14:00:31 +0200

    Move video embed script and tracking code to files
    Make CSP safe with no unsafe-inline

--- frontend/javascript/misc.js
+++ frontend/javascript/misc.js
@@ -10,4 +10,21 @@ window.document.addEventListener('securitypolicyviolation', (e) => {
   if (e.violatedDirective.indexOf('script') !== -1 && e.blockedURI.indexOf('https://data1.') !== -1) {
     window.alert('Liebe/r Nutzer/in,\noffenbar versucht eine Ihrer Browser-Erweiterungen Sie vermutlich ohne Ihre Kenntnis zu tracken.\n\nWir raten Ihnen Browser-Erweiterungen zu deinstallieren, die Sie nicht nutzen.')
   }
-})
+});
+
+(function () {
+  const videoEmbed = document.querySelector('.video-embed')
+  if (videoEmbed !== null) {
+    videoEmbed.addEventListener('click', function (e) {
+      e.preventDefault()
+      this.parentNode.innerHTML = '<iframe src="' + this.dataset.url + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+    })
+  }
+}())
+
+window._paq = window._paq || []
+window._paq.push(['setDomains', ['*.fragdenstaat.de']])
+window._paq.push(['trackPageView'])
+window._paq.push(['enableLinkTracking'])
+window._paq.push(['setTrackerUrl', 'https://traffic.okfn.de/piwik.php'])
+window._paq.push(['setSiteId', '25'])
