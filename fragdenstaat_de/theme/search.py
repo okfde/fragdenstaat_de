f5eadae00de326ffd7ae53c2f31a0ce5adc1cd81
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-27 15:22:16 +0100

    Change ES text analyzer

--- fragdenstaat_de/theme/search.py
+++ fragdenstaat_de/theme/search.py
@@ -8,29 +8,31 @@ def get_text_analyzer():
         'fds_analyzer',
         tokenizer='standard',
         filter=[
-            'standard',
-            'lowercase',
             'keyword_repeat',
-            token_filter('stop_de', type='stop', stopwords="_german_"),
-            'asciifolding',
-            # 'word_delimiter', # Breaks indexing
-            token_filter('decomp', type='decompound'),
-            token_filter('de_stemmer', type='stemmer', name='light_german'),
-            token_filter('unique_stem', type='unique', only_on_same_position=True)
-        ],
-    )
+            token_filter('decomp', type='decompound', subwords_only=True),
 
-
-def get_search_analyzer():
-    return analyzer(
-        'fds_search_analyzer',
-        tokenizer='standard',
-        filter=[
-            'standard',
             'lowercase',
             token_filter('stop_de', type='stop', stopwords="_german_"),
+
+            'german_normalization',
             'asciifolding',
+
             token_filter('de_stemmer', type='stemmer', name='light_german'),
             token_filter('unique_stem', type='unique', only_on_same_position=True)
         ],
     )
+
+
+# def get_search_analyzer():
+#     return analyzer(
+#         'fds_search_analyzer',
+#         tokenizer='standard',
+#         filter=[
+#             'standard',
+#             'lowercase',
+#             token_filter('stop_de', type='stop', stopwords="_german_"),
+#             'asciifolding',
+#             token_filter('de_stemmer', type='stemmer', name='light_german'),
+#             token_filter('unique_stem', type='unique', only_on_same_position=True)
+#         ],
+#     )
