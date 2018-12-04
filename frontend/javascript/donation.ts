d3635fccc9f3df11137a526c152aa706e2173af5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-12-04 15:56:45 +0100

    Use toggleSlide from froide misc

--- frontend/javascript/donation.ts
+++ frontend/javascript/donation.ts
@@ -1,64 +1,6 @@
+import { toggleSlide } from 'froide/frontend/javascript/lib/misc'
+
 interface DonationBannerStore { timestamp: number; }
-
-const getHeight = function (el: HTMLElement) {
-  var style = window.getComputedStyle(el)
-  var display = style.display
-  var position = style.position
-  var visibility = style.visibility
-  var maxHeight = style.maxHeight && style.maxHeight.replace('px', '').replace('%', '')
-  var wantedHeight = 0
-
-  // if its not hidden we just return normal height
-  if (display !== 'none' && maxHeight !== '0') {
-    return el.offsetHeight
-  }
-
-  // the element is hidden so:
-  // making the el block so we can meassure its height but still be hidden
-  el.style.position = 'absolute'
-  el.style.visibility = 'hidden'
-  el.style.display = 'block'
-
-  wantedHeight = el.offsetHeight
-
-  // reverting to the original values
-  el.style.display = display
-  el.style.position = position
-  el.style.visibility = visibility
-
-  return wantedHeight
-}
-
-/**
-* toggleSlide mimics the jQuery version of slideDown and slideUp
-* all in one function comparing the max-heigth to 0
-  */
-var toggleSlide = function (el: HTMLElement) {
-  var elMaxHeight = '0px'
-
-  if (el.getAttribute('data-max-height')) {
-    // we've already used this before, so everything is setup
-    let maxHeight = el.style.maxHeight
-    maxHeight = maxHeight && maxHeight.replace('px', '').replace('%', '')
-    if (maxHeight === '0') {
-      el.style.maxHeight = el.getAttribute('data-max-height')
-    } else {
-      el.style.maxHeight = '0'
-    }
-  } else {
-    elMaxHeight = getHeight(el) + 'px'
-    el.style['transition'] = 'max-height 2s ease-in-out'
-    el.style.overflowY = 'hidden'
-    el.style.maxHeight = '0'
-    el.setAttribute('data-max-height', elMaxHeight)
-    el.style.display = 'block'
-
-    // we use setTimeout to modify maxHeight later than display (so we have the transition effect)
-    setTimeout(function () {
-      el.style.maxHeight = elMaxHeight
-    }, 10)
-  }
-}
 
 function showDonationBanner () {
   window._paq = window._paq || []
