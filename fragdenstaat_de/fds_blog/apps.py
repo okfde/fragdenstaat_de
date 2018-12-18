151cd0d88611bb7dfb67de79687479f6c04c1a7f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-12-18 19:21:48 +0100

    Monkey patch Filer's AdminFileWidget media

--- fragdenstaat_de/fds_blog/apps.py
+++ fragdenstaat_de/fds_blog/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     name = 'fragdenstaat_de.fds_blog'
     verbose_name = 'FragDenStaat Blog'
+
+    def ready(self):
+        from filer.fields.file import AdminFileWidget
+
+        AdminFileWidget.Media.js = (
+            'admin/js/vendor/jquery/jquery.js',
+            'admin/js/jquery.init.js',
+        ) + AdminFileWidget.Media.js
