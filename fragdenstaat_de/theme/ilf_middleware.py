a62715e751dad49988c7b84d3ac4b6fec77bfcf4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-08 15:33:25 +0000

    Port ilf middleware to Django 2.1

--- fragdenstaat_de/theme/ilf_middleware.py
+++ fragdenstaat_de/theme/ilf_middleware.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 
 from django.conf import settings
-from django.utils.encoding import force_text
+from django.core.exceptions import DisallowedHost
 from django.utils.http import is_same_domain
 from django.middleware.csrf import (CsrfViewMiddleware, _sanitize_token,
                                     _compare_salted_tokens,
@@ -21,11 +21,6 @@ class CsrfViewIlfMiddleware(CsrfViewMiddleware):
     def process_view(self, request, callback, callback_args, callback_kwargs):
         if getattr(request, 'csrf_processing_done', False):
             return None
-
-        csrf_token = self._get_token(request)
-        if csrf_token is not None:
-            # Use same token next time.
-            request.META['CSRF_COOKIE'] = csrf_token
 
         # Wait until request.META["CSRF_COOKIE"] has been manipulated before
         # bailing out, so that get_token still works
@@ -58,11 +53,8 @@ class CsrfViewIlfMiddleware(CsrfViewMiddleware):
                 # Barth et al. found that the Referer header is missing for
                 # same-domain requests in only about 0.2% of cases or less, so
                 # we can use strict Referer checking.
-                referer = force_text(
-                    request.META.get('HTTP_REFERER'),
-                    strings_only=True,
-                    errors='replace'
-                )
+                referer = request.META.get('HTTP_REFERER')
+
                 # -- Change from original here -- #
                 # Only checks referer if it is present.
                 # It's not a failure condition if the referer is not present.
@@ -92,19 +84,23 @@ class CsrfViewIlfMiddleware(CsrfViewMiddleware):
                         if server_port not in ('443', '80'):
                             good_referer = '%s:%s' % (good_referer, server_port)
                     else:
-                        # request.get_host() includes the port.
-                        good_referer = request.get_host()
+                        try:
+                            # request.get_host() includes the port.
+                            good_referer = request.get_host()
+                        except DisallowedHost:
+                            pass
 
-                    # Here we generate a list of all acceptable HTTP referers,
-                    # including the current host since that has been validated
-                    # upstream.
+                    # Create a list of all acceptable HTTP referers, including the
+                    # current host if it's permitted by ALLOWED_HOSTS.
                     good_hosts = list(settings.CSRF_TRUSTED_ORIGINS)
-                    good_hosts.append(good_referer)
+                    if good_referer is not None:
+                        good_hosts.append(good_referer)
 
                     if not any(is_same_domain(referer.netloc, host) for host in good_hosts):
                         reason = REASON_BAD_REFERER % referer.geturl()
                         return self._reject(request, reason)
 
+            csrf_token = request.META.get('CSRF_COOKIE')
             if csrf_token is None:
                 # No CSRF cookie. For POST requests, we insist on a CSRF cookie,
                 # and in this way we can avoid all CSRF attacks, including login
