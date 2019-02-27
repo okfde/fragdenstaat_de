76d34f7d3b95ef589882b633a8b78e4f6b8a1764
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-02-27 13:45:57 +0100

    Add fds_blog filters file

---
+++ fragdenstaat_de/fds_blog/filters.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+import django_filters
+
+from froide.helper.search.filters import BaseSearchFilterSet
+
+from .models import Category, Author
+
+
+class ArticleFilterset(BaseSearchFilterSet):
+    query_fields = ['title^5', 'description^3', 'content']
+
+    category = django_filters.ModelChoiceFilter(
+        queryset=Category.objects.all(),
+        empty_label=_('all categories'),
+        widget=forms.Select(
+            attrs={
+                'label': _('category'),
+                'class': 'form-control'
+            }
+        ),
+        method='filter_category'
+    )
+
+    author = django_filters.ModelChoiceFilter(
+        queryset=Author.objects.all(),
+        empty_label=_('all authors'),
+        widget=forms.Select(
+            attrs={
+                'label': _('author'),
+                'class': 'form-control'
+            }
+        ),
+        method='filter_author'
+    )
+
+    def filter_category(self, qs, name, value):
+        return qs.filter(category=value.id)
+
+    def filter_author(self, qs, name, value):
+        return qs.filter(author=value.id)
