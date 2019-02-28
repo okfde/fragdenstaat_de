4f807d38a71f79531d43a308b311835c2b148bd0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-02-28 11:37:10 +0100

    Catch error with non-existing page extension

--- fragdenstaat_de/fds_cms/listeners.py
+++ fragdenstaat_de/fds_cms/listeners.py
@@ -10,7 +10,12 @@ from froide.helper.tasks import (
 @receiver(post_publish, dispatch_uid='publish_cms_page')
 def publish_cms_page(sender, instance, language, **kwargs):
     title = instance.publisher_public.get_title_obj(language)
-    if instance.fdspageextension.search_index:
+    try:
+        search_index = instance.fdspageextension.search_index
+    except Exception:
+        # In case page extension does not exist yet, assume indexing is OK
+        search_index = True
+    if search_index:
         search_instance_save.delay(title._meta.label_lower, title.pk)
     else:
         search_instance_delete.delay(title._meta.label_lower, title.pk)
