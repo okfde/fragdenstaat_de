ae4a07d03c128cc6f87ae64ba6fa7ad84e45d31a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-03-20 11:36:58 +0100

    Add monkey patch of get_language_from_path
    To interpret missing lanuage prefix as default language

--- fragdenstaat_de/theme/models.py
+++ fragdenstaat_de/theme/models.py
@@ -5,9 +5,31 @@ from django.shortcuts import redirect
 from django.core.cache import cache
 from django.utils import timezone
 from django.core.mail import mail_managers
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import trans_real
 
 from froide.foirequest.hooks import registry
+
+
+orig_get_language_from_path = trans_real.get_language_from_path
+
+
+def wrapping_get_language_from_path(*args, **kwargs):
+    '''
+    Monkey patch
+    If no language can be detected from path
+    it means the default language should be used.
+    The current method in Django doesn't support
+    i18n_patterns prefix_default_language=False
+    '''
+    result = orig_get_language_from_path(*args, **kwargs)
+    if result is None:
+        return settings.LANGUAGE_CODE
+    return result
+
+
+trans_real.get_language_from_path = wrapping_get_language_from_path
 
 
 def detect_troll_pre_request_creation(request, **kwargs):
