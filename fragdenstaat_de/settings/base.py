8e3c57ff1db3bb2a161112d402055c8a747a3dbd
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-03-28 16:10:25 +0100

    Add more greetings, reformat closings

--- fragdenstaat_de/settings/base.py
+++ fragdenstaat_de/settings/base.py
@@ -372,13 +372,20 @@ class FragDenStaatBase(German, Base):
             ],
             greetings=[
                 rec(r"Sehr geehrte Damen und Herren,?"),
+                rec(r"Hallo,?"),
+                rec(r"Sehr (?:Herr|Frau|Fr\.|Hr\.) .*"),
                 rec(r"Sehr geehrt(er? (?:Herr|Frau|Fr\.|Hr\.)?(?: ?Dr\.?)?(?: ?Prof\.?)? .*)"),
                 rec(r"^(?:Von|An|Cc|To|From): .*"),
             ],
             custom_replacements=[
                 rec(r'[Bb][Gg]-[Nn][Rr]\.?\s*\:?\s*([a-zA-Z0-9\s/]+)')
             ],
-            closings=[rec(r"([Mm]it )?(den )?(freundliche(n|m)?|vielen|besten)? ?Gr(ü|u)(ß|ss)(en?)?,?"), rec("Hochachtungsvoll,?"), rec(r'i\. ?A\.'), rec('[iI]m Auftrag')],
+            closings=[
+                rec(r"([Mm]it )?(den )?(freundliche(n|m)?|vielen|besten)? ?Gr(ü|u)(ß|ss)(en?)?,?"),
+                rec("Hochachtungsvoll,?"),
+                rec(r'i\. ?A\.'),
+                rec('[iI]m Auftrag')
+            ],
             content_urls={
                 'terms': '/nutzungsbedingungen/',
                 'privacy': '/datenschutzerklaerung/',
