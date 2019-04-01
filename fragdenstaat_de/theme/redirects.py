7ae53183675b330815bf51032b73cb9e7b1f7e91
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-04-01 13:58:48 +0200

    Add better query string handling

--- fragdenstaat_de/theme/redirects.py
+++ fragdenstaat_de/theme/redirects.py
@@ -3,12 +3,15 @@ from django.contrib.redirects.models import Redirect
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.redirects.middleware import RedirectFallbackMiddleware
 
+from froide.helper.utils import update_query_params
+
 
 class PathRedirectFallbackMiddleware(RedirectFallbackMiddleware):
     '''
     A custom version of Djangos RedirectFallbackMiddleware
     that looks at request.path instead of request.get_full_path(),
-    so it ignores any query strings and just looks at path
+    so it ignores any query strings and just looks at path.
+    It then merges querystrings of request and new path.
     '''
     def process_response(self, request, response):
         # No need to check for a redirect for non-404 responses.
@@ -34,7 +37,8 @@ class PathRedirectFallbackMiddleware(RedirectFallbackMiddleware):
         if r is not None:
             if r.new_path == '':
                 return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
+            full_path = update_query_params(r.new_path, request.GET)
+            return self.response_redirect_class(full_path)
 
         # No redirect was found. Return the response.
         return response
