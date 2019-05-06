1c977c3044e85aa561abee6b98bf88d15233c8f8
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-05-06 13:26:26 +0200

    Only show glyphocat on valid submit

--- frontend/javascript/request.ts
+++ frontend/javascript/request.ts
@@ -2,9 +2,9 @@ import 'froide/frontend/javascript/request.ts'
 
 
 const container = document.querySelector('#glyphosat-download .modal-content')
-const el = document.querySelector('#glyphosat-download-button')
+const el = document.querySelector('#glyphosat-download-form')
 if (container && el) {
-  el.addEventListener('click', () => {
+  el.addEventListener('submit', () => {
     window.setTimeout(() => {
       container.innerHTML = '<div class="text-center"><h3>Gutachten wird geladen...</h3><img src="https://media.frag-den-staat.de/files/media/main/13/2c/132cf116-f09b-4498-8429-2cbbf96b9f8b/keyboard-cat.gif" alt="Loading"/></div>'
     }, 300)
