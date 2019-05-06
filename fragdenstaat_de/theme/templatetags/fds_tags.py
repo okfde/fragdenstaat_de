d3037ac4dd827e685ae8d15c5e5aa33cf3e087b4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-05-06 14:32:07 +0200

    Use nicer template tag for glyphosat button logic

---
+++ fragdenstaat_de/theme/templatetags/fds_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+from ..glyphosat import FILENAME
+
+register = template.Library()
+
+
+@register.filter
+def needs_glyphosat_attachment(message):
+    if not (
+            message.request.campaign_id == 9 and
+            message.is_response and
+            'Kennwort lautet:' in message.plaintext):
+        return False
+    if any(a.name == FILENAME for a in message.attachments):
+        return False
+    return True
