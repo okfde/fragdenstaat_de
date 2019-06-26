2a07e1a4a609865da7e8600ff82b1d094edafe53
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-06-26 17:01:33 +0200

    Increase DATA_UPLOAD_MAX_NUMBER_FIELDS

--- fragdenstaat_de/settings/production.py
+++ fragdenstaat_de/settings/production.py
@@ -28,6 +28,7 @@ class FragDenStaat(FragDenStaatBase):
     SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
     DATA_UPLOAD_MAX_MEMORY_SIZE = 15728640  # 15 MB
+    DATA_UPLOAD_MAX_NUMBER_FIELDS = 5000
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
     STATIC_URL = 'https://static.frag-den-staat.de/static/'
 
