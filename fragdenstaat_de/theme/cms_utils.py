702267d3381ec8ef0aaa41dc9ef5c2800fbc1397
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-08 14:40:46 +0200

    Add doc string

--- fragdenstaat_de/theme/cms_utils.py
+++ fragdenstaat_de/theme/cms_utils.py
@@ -4,6 +4,7 @@ from cms.middleware.language import LanguageCookieMiddleware
 
 class HostLanguageCookieMiddleware(LanguageCookieMiddleware):
     def process_response(self, request, response):
+        ''' Don't set django_language cookie on non-primary domain'''
         if request.META['HTTP_HOST'] != settings.ALLOWED_HOSTS[0]:
             return response
         return super().process_response(request, response)
