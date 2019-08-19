aa70a0dde30cbddfb8f19e069a2a762a2e20a75c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-19 13:56:50 +0200

    Add blog cms toolbar

---
+++ fragdenstaat_de/fds_blog/cms_toolbars.py
@@ -0,0 +1,24 @@
+"""Toolbar extensions for CMS"""
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from cms.toolbar_base import CMSToolbar
+from cms.toolbar_pool import toolbar_pool
+
+
+class BlogToolbar(CMSToolbar):
+
+    def populate(self):
+        print(self.request.article)
+        if not hasattr(self.request, 'article'):
+            return
+
+        blog_menu = self.toolbar.get_or_create_menu(
+            'blog-menu', _('Blog')
+        )
+        article = self.request.article
+        url = reverse('admin:fds_blog_article_change', args=(article.pk,))
+        blog_menu.add_modal_item(_('Edit article'), url=url)
+
+
+toolbar_pool.register(BlogToolbar)
