56a19edb26fbaadeffe44e8448133efbb7ce4133
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-09-28 09:57:28 +0200

    Add messages to make order view

--- fragdenstaat_de/fds_donation/views.py
+++ fragdenstaat_de/fds_donation/views.py
@@ -1,4 +1,5 @@
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 from froide.helper.utils import get_redirect
 
@@ -10,5 +11,7 @@ def make_order(request, category):
     form = DonationGiftForm(data=request.POST, category=category)
     if form.is_valid():
         form.save(request)
+        return get_redirect(request)
 
-    return get_redirect(request)
+    messages.add_message(request, messages.ERROR, 'Form-Fehler!')
+    return get_redirect(request, next=request.META.get('HTTP_REFERER', '/'))
