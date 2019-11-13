8d7ad94e2a3651179ed74a8ea774dde014d7bde0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-11-13 11:35:33 +0100

    Fix typescript to tslint

--- frontend/javascript/donation-form.ts
+++ frontend/javascript/donation-form.ts
@@ -1,167 +1,167 @@
-import { toggleSlide } from 'froide/frontend/javascript/lib/misc'
+import { toggleSlide } from "froide/frontend/javascript/lib/misc";
 
-interface ApplePaySession {
-  canMakePayments(): boolean
+interface IApplePaySession {
+  canMakePayments(): boolean;
 }
 
 declare global {
+  // tslint:disable-next-line
   interface Window {
-    ApplePaySession: ApplePaySession | undefined
+    ApplePaySession: IApplePaySession | undefined;
   }
 }
 
-function setupDonationForm (form: HTMLFormElement) {
-  const amountGroup = form.querySelector('.amount-group')
+function setupDonationForm(form: HTMLFormElement) {
+  const amountGroup = form.querySelector(".amount-group");
   if (amountGroup !== null) {
-    setupAmountGroup(amountGroup)
+    setupAmountGroup(amountGroup);
   }
   const radioCollapse = form.querySelectorAll('[data-toggle="radiocollapse"]');
-  (<HTMLInputElement[]>Array.from(radioCollapse)).forEach(setupRadioCollapse);
-  const intervalGroup = document.getElementById('id_interval')
+  (Array.from(radioCollapse) as HTMLInputElement[]).forEach(setupRadioCollapse);
+  const intervalGroup = document.getElementById("id_interval");
   if (intervalGroup !== null) {
-    setupIntervalGroup(intervalGroup)
+    setupIntervalGroup(intervalGroup);
   }
-  const additionalCCProviders = []
+  const additionalCCProviders = [];
   if (window.ApplePaySession && window.ApplePaySession.canMakePayments) {
-    additionalCCProviders.push('Apple Pay')
+    additionalCCProviders.push("Apple Pay");
   }
   if (additionalCCProviders.length > 0) {
-    const ccInput = document.querySelector('input[value="creditcard"]')
+    const ccInput = document.querySelector('input[value="creditcard"]');
     if (ccInput && ccInput.parentElement) {
-      const parent = ccInput.parentElement
-      parent.childNodes[1].textContent += ' / ' + additionalCCProviders.join(' / ')
+      const parent = ccInput.parentElement;
+      parent.childNodes[1].textContent += " / " + additionalCCProviders.join(" / ");
     }
   }
 }
 
 function setupIntervalGroup(intervalGroup: HTMLElement) {
-  const oneTimePaymentMethods = (<HTMLInputElement[]>Array.from(
-    document.querySelectorAll('#id_payment_method input[value="sofort"]')
-  ));
-  const oneTimeFields = (<HTMLInputElement[]>Array.from(
-    document.querySelectorAll('[data-toggle="nonrecurring"]')
-  ));
-  const inputs = (<HTMLInputElement[]>Array.from(intervalGroup.querySelectorAll('input')));
+  const oneTimePaymentMethods = (Array.from(
+    document.querySelectorAll('#id_payment_method input[value="sofort"]'),
+  ) as HTMLInputElement[]);
+  const oneTimeFields = (Array.from(
+    document.querySelectorAll('[data-toggle="nonrecurring"]'),
+  ) as HTMLInputElement[]);
+  const inputs = (Array.from(intervalGroup.querySelectorAll("input")) as HTMLInputElement[]);
   inputs.forEach((input) => {
-    input.addEventListener('change', () => {
-      const oneTime = input.value === '0'
-      toggleRadioInput(oneTimePaymentMethods, oneTime)
+    input.addEventListener("change", () => {
+      const isOneTime = input.value === "0";
+      toggleRadioInput(oneTimePaymentMethods, isOneTime);
       oneTimeFields.forEach((el) => {
-        if (!oneTime) {
-          el.value = ''
-          el.disabled = true
+        if (!isOneTime) {
+          el.value = el.querySelectorAll("option")[0].value;
+          el.disabled = true;
         } else {
-          el.disabled = false
+          el.disabled = false;
         }
-      })
-    })
-  })
-  const oneTime = <HTMLInputElement>document.getElementById('id_interval_0')
+      });
+    });
+  });
+  const oneTime = document.getElementById("id_interval_0") as HTMLInputElement;
   if (!oneTime || !oneTime.checked) {
-    toggleRadioInput(oneTimePaymentMethods, false)
-    oneTimeFields.forEach((el) => el.classList.toggle('collapse', false))
+    toggleRadioInput(oneTimePaymentMethods, false);
+    oneTimeFields.forEach((el) => el.classList.toggle("collapse", false));
   }
 }
 
 function toggleRadioInput(inputs: HTMLInputElement[], checked: boolean) {
   inputs.forEach((input) => {
     if (!checked && input.checked) {
-      input.checked = checked
+      input.checked = checked;
     }
-    input.disabled = !checked
+    input.disabled = !checked;
     // Hide parent label element
     if (input.parentElement) {
-      input.parentElement.classList.toggle('collapse', !checked)
+      input.parentElement.classList.toggle("collapse", !checked);
     }
-  })
+  });
 }
 
-function setupRadioCollapse (radioCollapse: HTMLInputElement) {
-  const targetId = radioCollapse.dataset.target
+function setupRadioCollapse(radioCollapse: HTMLInputElement) {
+  const targetId = radioCollapse.dataset.target;
   if (!targetId) {
-    return
+    return;
   }
-  const target = document.getElementById(targetId)
+  const target = document.getElementById(targetId);
   if (target === null) {
-    return
+    return;
   }
-  radioCollapse.addEventListener('change', () => {
-    toggleRadioCollapse(radioCollapse, target)
-  })
+  radioCollapse.addEventListener("change", () => {
+    toggleRadioCollapse(radioCollapse, target);
+  });
 }
 
-function toggleRadioCollapse (input: HTMLInputElement, target: HTMLElement) {
-  if (input.value === '1') {
-    target.classList.add('show')
+function toggleRadioCollapse(input: HTMLInputElement, target: HTMLElement) {
+  if (input.value === "1") {
+    target.classList.add("show");
     toggleSlide(target, 0.5);
-    (<HTMLInputElement[]>Array.from(target.querySelectorAll('input,select'))).forEach((el) => {
-      el.required = true
-    })
+    (Array.from(target.querySelectorAll("input,select")) as HTMLInputElement[]).forEach((el) => {
+      el.required = true;
+    });
   } else {
-    target.classList.remove('show')
+    target.classList.remove("show");
     toggleSlide(target, 0.5);
-    (<HTMLInputElement[]>Array.from(target.querySelectorAll('input,select'))).forEach((el) => {
-      el.required = false
-    })
+    (Array.from(target.querySelectorAll("input,select")) as HTMLInputElement[]).forEach((el) => {
+      el.required = false;
+    });
   }
 }
 
-function setupAmountGroup (amountGroup: Element) {
-  const input = amountGroup.querySelector('input')
-  const buttons = <HTMLButtonElement[]>Array.from(amountGroup.querySelectorAll('button'))
+function setupAmountGroup(amountGroup: Element) {
+  const input = amountGroup.querySelector("input");
+  const buttons = Array.from(amountGroup.querySelectorAll("button")) as HTMLButtonElement[];
 
   if (input) {
-    const focusClicks = (<HTMLElement[]>Array.from(amountGroup.querySelectorAll('[data-focus]')));
+    const focusClicks = (Array.from(amountGroup.querySelectorAll("[data-focus]")) as HTMLElement[]);
     focusClicks.forEach((focusClick) => {
-      focusClick.addEventListener('click', () => {
-        input.focus()
-      })
-    })
+      focusClick.addEventListener("click", () => {
+        input.focus();
+      });
+    });
   }
 
-  function highlightButtonWithValue (value: string) {
-    let any = false
+  function highlightButtonWithValue(value: string) {
+    let hasAny = false;
     buttons.forEach((button) => {
       if (button.dataset.value === value) {
-        any = true
-        button.classList.add('btn-primary')
-        button.classList.remove('btn-outline-primary')
+        hasAny = true;
+        button.classList.add("btn-primary");
+        button.classList.remove("btn-outline-primary");
       } else {
-        button.classList.remove('btn-primary')
-        button.classList.add('btn-outline-primary')
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-outline-primary");
       }
-    })
-    return any
+    });
+    return hasAny;
   }
 
-  function buttonClick (button: HTMLButtonElement) {
-    const value = button.dataset.value || ''
+  function buttonClick(button: HTMLButtonElement) {
+    const value = button.dataset.value || "";
     if (input !== null) {
-      input.value = value
+      input.value = value;
     }
-    highlightButtonWithValue(value)
+    highlightButtonWithValue(value);
   }
-  function inputChanged (input: HTMLInputElement) {
-    const anyButton = highlightButtonWithValue(input.value)
+  function inputChanged(amountInput: HTMLInputElement) {
+    const anyButton = highlightButtonWithValue(amountInput.value);
     if (!anyButton) {
-      input.classList.add('border', 'border-primary')
+      amountInput.classList.add("border", "border-primary");
     } else {
-      input.classList.remove('border', 'border-primary')
+      amountInput.classList.remove("border", "border-primary");
     }
   }
 
   buttons.forEach((button) => {
-    button.addEventListener('click', () => buttonClick(button))
-  })
+    button.addEventListener("click", () => buttonClick(button));
+  });
   if (input) {
-    input.addEventListener('focus', () => inputChanged(input))
-    input.addEventListener('keyup', () => inputChanged(input))
-    input.addEventListener('change', () => inputChanged(input))
+    input.addEventListener("focus", () => inputChanged(input));
+    input.addEventListener("keyup", () => inputChanged(input));
+    input.addEventListener("change", () => inputChanged(input));
   }
 }
 
-
-const donationForm = document.querySelector('.donation-form')
+const donationForm = document.querySelector(".donation-form");
 if (donationForm !== null) {
-  setupDonationForm(<HTMLFormElement>donationForm)
+  setupDonationForm(donationForm as HTMLFormElement);
 }
--- frontend/javascript/donation.ts
+++ frontend/javascript/donation.ts
@@ -1,105 +1,105 @@
-import { toggleSlide } from 'froide/frontend/javascript/lib/misc'
+import { toggleSlide } from "froide/frontend/javascript/lib/misc";
 
-interface DonationBannerStore { timestamp: number; }
+interface IDonationBannerStore { timestamp: number; }
 
-function showDonationBanner () {
-  window._paq = window._paq || []
+function showDonationBanner() {
+  window._paq = window._paq || [];
 
-  function hideBanner (code: string, time: number) {
-    return function (e: Event) {
-      e.preventDefault()
-      window._paq.push(['trackEvent', 'donations', 'donationBanner', code])
+  function hideBanner(code: string, time: number) {
+    return (e: Event) => {
+      e.preventDefault();
+      window._paq.push(["trackEvent", "donations", "donationBanner", code]);
       window.localStorage.setItem(itemName, JSON.stringify({
-        timestamp: time
-      }))
+        timestamp: time,
+      }));
       if (requiresAnimation) {
-        toggleSlide(el)
-        window.setTimeout(function () {
-          removeBanner()
-        }, 5 * 1000)
+        toggleSlide(el);
+        window.setTimeout(() => {
+          removeBanner();
+        }, 5 * 1000);
       } else {
-        removeBanner()
+        removeBanner();
       }
-    }
+    };
   }
   function removeBanner() {
-    el.parentNode && el.parentNode.removeChild(el)
-  }
-
-  const els = document.querySelectorAll('.donation-block')
-  if (els.length === 2) {
-    /* Show second banner like normal */
-    let demoEl = <HTMLElement> els[1]
-    demoEl.style.display = 'block'
-    return
-  }
-  if (els.length !== 1) {
-    return
-  }
-  const el = <HTMLElement> els[0]
-
-  if (document.location && document.location.pathname) {
-    if (document.location.pathname.indexOf('/spenden/') !== -1 ||
-        document.location.pathname.indexOf('/blog/') !== -1 ||
-        document.location.pathname.indexOf('/gesendet/') !== -1 ||
-        document.location.pathname.indexOf('/anfrage-stellen/') !== -1 ||
-        document.location.pathname.indexOf('/account/') !== -1) {
-      return removeBanner()
+    if (el.parentNode) {
+      el.parentNode.removeChild(el);
     }
   }
-  const donationIframe = document.querySelector('iframe[src^="https://www.betterplace.org/de/projects/"]')
+
+  const els = document.querySelectorAll(".donation-block");
+  if (els.length === 2) {
+    /* Show second banner like normal */
+    const demoEl = els[1] as HTMLElement;
+    demoEl.style.display = "block";
+    return;
+  }
+  if (els.length !== 1) {
+    return;
+  }
+  const el = els[0] as HTMLElement;
+
+  if (document.location && document.location.pathname) {
+    if (document.location.pathname.indexOf("/spenden/") !== -1 ||
+        document.location.pathname.indexOf("/blog/") !== -1 ||
+        document.location.pathname.indexOf("/gesendet/") !== -1 ||
+        document.location.pathname.indexOf("/anfrage-stellen/") !== -1 ||
+        document.location.pathname.indexOf("/account/") !== -1) {
+      return removeBanner();
+    }
+  }
+  const donationIframe = document.querySelector('iframe[src^="https://www.betterplace.org/de/projects/"]');
   if (donationIframe !== null) {
-    return removeBanner()
+    return removeBanner();
   }
 
   if (!window.localStorage) {
-    return removeBanner()
+    return removeBanner();
   }
-  const requiresAnimation = el.dataset.topbanner === "true"
-  const cancel = el.querySelector('.cancel-donation')
-  const already = el.querySelector('.already-donation')
-  const close = el.querySelector('.close')
+  const requiresAnimation = el.dataset.topbanner === "true";
+  const cancel = el.querySelector(".cancel-donation");
+  const already = el.querySelector(".already-donation");
+  const close = el.querySelector(".close");
 
-  const itemName = 'donation-banner'
-  const now = (new Date()).getTime()
-  const data = window.localStorage.getItem(itemName)
+  const itemName = "donation-banner";
+  const now = (new Date()).getTime();
+  const data = window.localStorage.getItem(itemName);
   if (data !== null) {
-    const last = <DonationBannerStore> JSON.parse(data)
+    const last = JSON.parse(data) as IDonationBannerStore;
     if (last.timestamp && (now - last.timestamp) < (60 * 60 * 24 * 1000)) {
-      return removeBanner()
+      return removeBanner();
     }
   }
-  window.localStorage.removeItem(itemName)
+  window.localStorage.removeItem(itemName);
 
   if (close) {
-    close.addEventListener('click', hideBanner('close', now))
+    close.addEventListener("click", hideBanner("close", now));
   }
 
   if (cancel) {
-    cancel.addEventListener('click', hideBanner('notnow', now))
+    cancel.addEventListener("click", hideBanner("notnow", now));
   }
   if (already) {
-    already.addEventListener('click', hideBanner('donated', now + (1000 * 60 * 60 * 24 * 30)))
+    already.addEventListener("click", hideBanner("donated", now + (1000 * 60 * 60 * 24 * 30)));
   }
   if (requiresAnimation) {
-    window.setTimeout(function () {
-      window._paq.push(['trackEvent', 'donations', 'donationBanner', 'shown'])
+    window.setTimeout(() => {
+      window._paq.push(["trackEvent", "donations", "donationBanner", "shown"]);
       if (window.innerWidth > 768) {
-        el.style.position = 'sticky'
-        el.style.top = '0'
+        el.style.position = "sticky";
+        el.style.top = "0";
       }
-      toggleSlide(el)
-      window.setTimeout(function () {
-      }, 2000)
-    }, 3000)
+      toggleSlide(el);
+    }, 3000);
   } else {
-    el.style.display = 'block'
-    window._paq.push(['trackEvent', 'donations', 'donationBanner', 'shown'])
+    el.style.display = "block";
+    window._paq.push(["trackEvent", "donations", "donationBanner", "shown"]);
   }
 }
 
-if (document.readyState === 'loading') {
-  window.document.addEventListener('DOMContentLoaded', showDonationBanner)
+if (document.readyState === "loading") {
+  window.document.addEventListener("DOMContentLoaded", showDonationBanner);
 } else {
-  showDonationBanner()
+  showDonationBanner();
 }
