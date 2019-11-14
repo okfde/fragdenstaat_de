1191031c0560007bd15723f3f273fb374fc626a9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-11-14 08:41:58 +0100

    Filter next read by published

--- fragdenstaat_de/fds_blog/templatetags/blog_tags.py
+++ fragdenstaat_de/fds_blog/templatetags/blog_tags.py
@@ -1,7 +1,7 @@
 from django import template
 
 from ..models import Article
-
+from ..managers import articles_published
 
 register = template.Library()
 
@@ -9,7 +9,7 @@ register = template.Library()
 @register.filter
 def get_next_read(article):
 
-    qs = Article.objects.all()
+    qs = articles_published(Article.objects.all())
     if article.start_publication:
         try:
             return qs.filter(start_publication__lt=article.start_publication)[0]
