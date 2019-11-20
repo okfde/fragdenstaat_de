3df850c1ab17fc6d170039f1c9dd95c4d882feb5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-11-20 14:00:26 +0100

    Add TSLint fix

--- frontend/javascript/main.ts
+++ frontend/javascript/main.ts
@@ -1,17 +1,17 @@
-import '../styles/main.scss'
+import "../styles/main.scss";
 
-import 'froide/frontend/javascript/main.ts'
+import "froide/frontend/javascript/main.ts";
 
-import './misc.ts'
-import './donation.ts'
-import './donation-form.ts'
-import './betterplace.ts'
-import './magnifier.ts'
+import "./betterplace.ts";
+import "./donation-form.ts";
+import "./donation.ts";
+import "./magnifier.ts";
+import "./misc.ts";
 
 if (document.body.dataset.raven) {
-  import(/* webpackChunkName: "@sentry/browser" */ '@sentry/browser').then((Sentry) => {
+  import(/* webpackChunkName: "@sentry/browser" */ "@sentry/browser").then((Sentry) => {
     Sentry.init({
-      dsn: document.body.dataset.raven
-    })
-  })
+      dsn: document.body.dataset.raven,
+    });
+  });
 }
