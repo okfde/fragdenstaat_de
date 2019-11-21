c1db576e9c5dc703ee1c75ca5ac9451e03481ac7
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-11-21 16:46:07 +0100

    Show donation banner on blog pages

--- frontend/javascript/donation.ts
+++ frontend/javascript/donation.ts
@@ -42,7 +42,6 @@ function showDonationBanner() {
 
   if (document.location && document.location.pathname) {
     if (document.location.pathname.indexOf("/spenden/") !== -1 ||
-        document.location.pathname.indexOf("/blog/") !== -1 ||
         document.location.pathname.indexOf("/gesendet/") !== -1 ||
         document.location.pathname.indexOf("/anfrage-stellen/") !== -1 ||
         document.location.pathname.indexOf("/account/") !== -1) {
