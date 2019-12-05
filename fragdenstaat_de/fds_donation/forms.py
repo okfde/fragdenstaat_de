5cbdc7a59836796ef4ad246e4bc2553daaa27678
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-12-05 16:12:22 +0100

    Set method on donation via form

--- fragdenstaat_de/fds_donation/forms.py
+++ fragdenstaat_de/fds_donation/forms.py
@@ -436,6 +436,7 @@ class DonationForm(StartPaymentMixin, AmountForm, DonorForm):
             reference=data.get('reference', ''),
             purpose=data.get('purpose', order.description),
             order=order,
+            method=data.get('payment_method', '')
         )
         return donation
 
