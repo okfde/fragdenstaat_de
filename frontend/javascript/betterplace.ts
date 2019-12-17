54d8b40e3b01439e3440d527456369148a1437cd
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-12-17 18:09:20 +0100

    Fix Typescript

--- frontend/javascript/betterplace.ts
+++ frontend/javascript/betterplace.ts
@@ -1,23 +1,23 @@
-const betterplaceProgress = document.querySelectorAll('.betterplace-progress')
+const betterplaceProgress = document.querySelectorAll(".betterplace-progress");
 
 Array.from(betterplaceProgress).forEach((el) => {
-  const dataset = (<HTMLElement> el).dataset
-  const bar = <HTMLElement> el.querySelector('.progress-bar')
+  const dataset = (el as HTMLElement).dataset;
+  const bar = el.querySelector(".progress-bar") as HTMLElement;
   if (bar === null) {
-    return
+    return;
   }
-  const url = dataset.url
+  const url = dataset.url;
   if (!url) {
-    return
+    return;
   }
-  window.fetch(url).then(response => response.json()).then((data) => {
-    bar.style.width = `${data.progress_percentage}%`
-    bar.textContent = `${Math.floor(parseFloat(data.progress_percentage))}%`
+  window.fetch(url).then((response) => response.json()).then((data) => {
+    bar.style.width = `${data.progress_percentage}%`;
+    bar.textContent = `${Math.floor(parseFloat(data.progress_percentage))}%`;
 
-    const openLabel = <HTMLElement> el.querySelector('.open-amount')
+    const openLabel = el.querySelector(".open-amount") as HTMLElement;
     if (openLabel) {
-      openLabel.textContent = `${Math.floor(data.open_amount_in_cents / 100)}`
+      openLabel.textContent = `${Math.floor(data.open_amount_in_cents / 100)}`;
     }
 
-  })
-})
+  });
+});
--- frontend/javascript/misc.ts
+++ frontend/javascript/misc.ts
@@ -10,56 +10,63 @@ window.addEventListener("message", (e) => {
 
 window.document.addEventListener("securitypolicyviolation", (e) => {
   if (e.violatedDirective.indexOf("script") !== -1 && e.blockedURI.indexOf("https://data1.") !== -1) {
-    window.alert("Liebe/r Nutzer/in,\noffenbar versucht eine Ihrer Browser-Erweiterungen Sie vermutlich ohne Ihre Kenntnis zu tracken.\n\nWir raten Ihnen Browser-Erweiterungen zu deinstallieren, die Sie nicht nutzen.");
+    window.alert(`Liebe/r Nutzer/in,\noffenbar versucht eine Ihrer Browser-Erweiterungen Sie vermutlich ohne
+    Ihre Kenntnis zu tracken.
+    \n\nWir raten Ihnen Browser-Erweiterungen zu deinstallieren, die Sie nicht nutzen.`);
   }
 });
 
-(function () {
-  const videoEmbed = document.querySelector(".video-embed");
-  if (videoEmbed !== null) {
-    videoEmbed.addEventListener("click", function(this: HTMLElement, e) {
-      e.preventDefault();
-      const parent = this.parentElement;
-      if (parent !== null && this.dataset.url) {
-        const iframe = document.createElement("iframe");
-        iframe.src = this.dataset.url;
-        iframe.setAttribute("frameBorder", "0");
-        iframe.setAttribute("webkitallowfullscreen", "");
-        iframe.setAttribute("mozallowfullscreen", "");
-        iframe.setAttribute("allowfullscreen", "");
-        parent.appendChild(iframe);
-        if (this.dataset.vimeosubtitlelanguage) {
-          const lang = this.dataset.vimeosubtitlelanguage;
-          const origin = this.dataset.url.split("/").slice(0, 3).join("/");
-          const setLanguage = () => {
-            if (!iframe.contentWindow) { return; }
-            iframe.contentWindow.postMessage({
-              method: "enableTextTrack",
-              value: {
-                kind: "subtitles",
-                language: lang,
-              },
-            }, origin);
-          };
-          iframe.addEventListener("load", setLanguage);
-          window.setTimeout(setLanguage, 1000);
-        }
+const videoEmbed = document.querySelector(".video-embed");
+if (videoEmbed !== null) {
+  videoEmbed.addEventListener("click", function(this: HTMLElement, e) {
+    e.preventDefault();
+    const parent = this.parentElement;
+    if (parent !== null && this.dataset.url) {
+      const iframe = document.createElement("iframe");
+      iframe.src = this.dataset.url;
+      iframe.setAttribute("frameBorder", "0");
+      iframe.setAttribute("webkitallowfullscreen", "");
+      iframe.setAttribute("mozallowfullscreen", "");
+      iframe.setAttribute("allowfullscreen", "");
+      parent.appendChild(iframe);
+      if (this.dataset.vimeosubtitlelanguage) {
+        const lang = this.dataset.vimeosubtitlelanguage;
+        const origin = this.dataset.url.split("/").slice(0, 3).join("/");
+        const setLanguage = () => {
+          if (!iframe.contentWindow) { return; }
+          iframe.contentWindow.postMessage({
+            method: "enableTextTrack",
+            value: {
+              kind: "subtitles",
+              language: lang,
+            },
+          }, origin);
+        };
+        iframe.addEventListener("load", setLanguage);
+        window.setTimeout(setLanguage, 1000);
       }
-    });
-  }
-}());
+    }
+  });
+}
 
+// tslint:disable-next-line: interface-name
 interface Window { _paq: Array<Array<string | string[]>>; }
 
-(function() {
-  const MATOMO_DOMAIN = "https://traffic.okfn.de";
+const MATOMO_DOMAIN = "https://traffic.okfn.de";
 
-  window._paq = window._paq || [];
-  window._paq.push(["trackPageView"]);
-  window._paq.push(["enableLinkTracking"]);
-  window._paq.push(["setDomains", ["*.fragdenstaat.de"]]);
-  window._paq.push(["setTrackerUrl", `${MATOMO_DOMAIN}/matomo.php`]);
-  window._paq.push(["setSiteId", "25"]);
+window._paq = window._paq || [];
+window._paq.push(["trackPageView"]);
+window._paq.push(["enableLinkTracking"]);
+window._paq.push(["setDomains", ["*.fragdenstaat.de"]]);
+window._paq.push(["setTrackerUrl", `${MATOMO_DOMAIN}/matomo.php`]);
+window._paq.push(["setSiteId", "25"]);
+
+if (document.location.hostname.indexOf(".onion") !== -1) {
+  document.body.classList.add("darkmode");
+  document.body.classList.add("onion-site");
+  const root = document.getElementsByTagName("html")[0];
+  root.setAttribute("class", "darkmode");
+} else {
 
   const script = document.createElement("script");
   script.type = "text/javascript";
@@ -67,11 +74,4 @@ interface Window { _paq: Array<Array<string | string[]>>; }
   script.defer = true;
   script.src = `${MATOMO_DOMAIN}/matomo.js`;
   document.body.appendChild(script);
-}());
-
-if (document.location.hostname.indexOf(".onion") !== -1) {
-  document.body.classList.add("darkmode");
-  document.body.classList.add("onion-site");
-  const root = document.getElementsByTagName("html")[0];
-  root.setAttribute("class", "darkmode");
 }
