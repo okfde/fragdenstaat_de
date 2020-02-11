402dab15195a1d0919f67437ea5c8d94ad45a64f
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-02-11 13:52:19 +0100

    mobile menu WIP

--- frontend/javascript/mobile-menu.ts
+++ frontend/javascript/mobile-menu.ts
@@ -2,6 +2,7 @@ const menuElement = document.querySelector('.mobile-menu')
 const toggleButton = document.querySelector('.mobile-menu__toggle')
 const menuContent = document.querySelector('.mobile-menu__wrapper') as HTMLElement
 const backdropElement = document.querySelector('.mobile-menu__backdrop') as HTMLElement
+const dropdownTriggers = document.querySelectorAll('.header-dropdown-trigger')
 
 function init() {
   // event for toggle button
@@ -9,6 +10,14 @@ function init() {
   
   // event for backdrop click
   backdropElement.addEventListener('click', closeMobileMenu)
+
+  // dropdown click events
+  for (let i = 0, l = dropdownTriggers.length; i < l; i++) {
+    const trigger: Element = dropdownTriggers[i]
+    trigger.addEventListener('click', (e) => {
+      triggerDrowdownMenu(e, trigger)
+    })
+  }
 }
 
 function isOpen () {
@@ -39,5 +48,42 @@ function openMobileMenu () {
   document.body.style.overflow = 'hidden'
   menuElement?.classList.add('mobile-menu--visible')
 }
+
+function triggerDrowdownMenu(e: Event, trigger: Element) {
+  e.preventDefault()
+
+  // find dropdown menu next to trigger
+  const menu: (Element | null) = trigger.nextElementSibling  
+
+  if (menu === null) return
+  if (dropdownIsOpen(menu)) closeDropdown(trigger, menu)
+  else openDropdown(trigger, menu)
+  // add border to trigger
+  // trigger.classList.add('border-bottom border-blue-800')
+  
+  
+}
+
+function dropdownIsOpen(dropdownElement: Element) {
+  return dropdownElement.classList.contains('header-dropdown-content--visible')
+}
+
+function openDropdown(trigger: Element, dropdownElement: Element) {
+  // add bg color
+  trigger.classList.add('bg-light')
+  dropdownElement.classList.add('bg-light')
+  // add visible class
+  dropdownElement.classList.add('header-dropdown-content--visible')
+}
+
+function closeDropdown(trigger: Element, dropdownElement: Element) {
+  // remove bg color
+  trigger.classList.remove('bg-light')
+  dropdownElement.classList.remove('bg-light')
+  // add visible class
+  dropdownElement.classList.remove('header-dropdown-content--visible')
+}
+
+
 
 init()
