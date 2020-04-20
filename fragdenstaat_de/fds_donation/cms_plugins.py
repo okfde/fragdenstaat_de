ac0737c2c3af5f4826ecb96845dbb3d649c0628a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-04-20 17:28:38 +0200

    Add keyword from pk_keyword to donation

--- fragdenstaat_de/fds_donation/cms_plugins.py
+++ fragdenstaat_de/fds_donation/cms_plugins.py
@@ -58,6 +58,7 @@ class DonationFormPlugin(CMSPluginBase):
         context['form'] = instance.make_form(
             user=request.user,
             reference=request.GET.get('pk_campaign', ''),
+            keyword=request.GET.get('pk_keyword', ''),
             action=instance.form_action or reverse('fds_donation:donate')
         )
         return context
