823517219da9698d8499e83013aa582157379e69
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-06-16 15:33:20 +0200

    init glide.js when document.readyState changes

--- frontend/javascript/slider.js
+++ frontend/javascript/slider.js
@@ -1,12 +1,16 @@
 import Glide from '@glidejs/glide'
 
-const glides = document.querySelectorAll('.glide')
-Array.from(glides).forEach((el) => {
-  let options = {}
-  if (el.dataset.options) {
-    try {
-      options = JSON.parse(el.dataset.options)
-    } catch {}
+document.onreadystatechange = function () {
+  if (document.readyState !== 'loading') {
+    const glides = document.querySelectorAll('.glide')
+    Array.from(glides).forEach((el) => {
+      let options = {}
+      if (el.dataset.options) {
+        try {
+          options = JSON.parse(el.dataset.options)
+        } catch {}
+      }
+      new Glide(el, options).mount()
+    })
   }
-  new Glide(el, options).mount()
-})
+}
