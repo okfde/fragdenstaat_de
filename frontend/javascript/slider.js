f1b4af54878ff839461cf5d6a4066c7ce4232071
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-06-16 17:04:37 +0200

    sliders: replace readystatechange event with window.onload

--- frontend/javascript/slider.js
+++ frontend/javascript/slider.js
@@ -1,16 +1,14 @@
 import Glide from '@glidejs/glide'
 
-document.onreadystatechange = function () {
-  if (document.readyState !== 'loading') {
-    const glides = document.querySelectorAll('.glide')
-    Array.from(glides).forEach((el) => {
-      let options = {}
-      if (el.dataset.options) {
-        try {
-          options = JSON.parse(el.dataset.options)
-        } catch {}
-      }
-      new Glide(el, options).mount()
-    })
-  }
+window.onload = () => {
+  const glides = document.querySelectorAll('.glide')
+  Array.from(glides).forEach((el) => {
+    let options = {}
+    if (el.dataset.options) {
+      try {
+        options = JSON.parse(el.dataset.options)
+      } catch {}
+    }
+    new Glide(el, options).mount()
+  })
 }
