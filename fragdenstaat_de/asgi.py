bbbf932f6cfe3e1f9b08c49809af064c1c546312
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-07-13 17:09:19 +0200

    Add asgi module

---
+++ fragdenstaat_de/asgi.py
@@ -0,0 +1,14 @@
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fragdenstaat_de.settings.development')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
+
+django.setup()
+application = get_default_application()
