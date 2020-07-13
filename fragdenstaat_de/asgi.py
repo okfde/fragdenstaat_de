e800f41e1275995ce693d5b4ad9fccca1e91dfe6
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-07-13 18:15:58 +0200

    Switch asgi to default dev environment

--- fragdenstaat_de/asgi.py
+++ fragdenstaat_de/asgi.py
@@ -7,11 +7,11 @@ import os
 import django
 from channels.routing import get_default_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fragdenstaat_de.settings.development')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+
 from configurations import importer
 importer.install()
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fragdenstaat_de.settings.production')
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
 django.setup()
 application = get_default_application()
