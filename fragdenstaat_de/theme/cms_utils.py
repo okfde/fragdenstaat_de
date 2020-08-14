0c464ed901913391c60d4e2f27626b6acf86ea07
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-08-14 17:52:29 +0200

    Don't rely on presence of HTTP_HOST on META

--- fragdenstaat_de/theme/cms_utils.py
+++ fragdenstaat_de/theme/cms_utils.py
@@ -5,6 +5,6 @@ from cms.middleware.language import LanguageCookieMiddleware
 class HostLanguageCookieMiddleware(LanguageCookieMiddleware):
     def process_response(self, request, response):
         ''' Don't set django_language cookie on non-primary domain'''
-        if request.META['HTTP_HOST'] != settings.ALLOWED_HOSTS[0]:
+        if request.META.get('HTTP_HOST') != settings.ALLOWED_HOSTS[0]:
             return response
         return super().process_response(request, response)
