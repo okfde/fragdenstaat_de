67a7371e9dfc7591b862e08097c517e557df0f60
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-12-22 21:11:59 +0000

    Fix ws consumers for channels 3.x

--- fragdenstaat_de/routing.py
+++ fragdenstaat_de/routing.py
@@ -9,7 +9,7 @@ from fragdenstaat_de.fds_cms.consumers import CMSPluginEditConsumer
 from froide.routing import websocket_urls as froide_ws_urls
 
 websocket_urls = froide_ws_urls + [
-    path('fds-cms/edit-plugin/<int:plugin_id>/', CMSPluginEditConsumer)
+    path('fds-cms/edit-plugin/<int:plugin_id>/', CMSPluginEditConsumer.as_asgi())
 ]
 
 application = ProtocolTypeRouter({
