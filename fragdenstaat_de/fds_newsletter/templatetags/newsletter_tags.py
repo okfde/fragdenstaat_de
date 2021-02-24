c4203c58b1966578bb3054007e3823570b26d896
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-02-24 11:25:33 +0000

    Refactor newsletter tags

--- fragdenstaat_de/fds_newsletter/templatetags/newsletter_tags.py
+++ fragdenstaat_de/fds_newsletter/templatetags/newsletter_tags.py
@@ -38,6 +38,29 @@ def newsletter_settings(context):
     }
 
 
+def _get_newsletter(newsletter_slug=None):
+    try:
+        return Newsletter.objects.get(
+            slug=newsletter_slug or settings.DEFAULT_NEWSLETTER
+        )
+    except Newsletter.DoesNotExist:
+        return None
+
+
+def _has_newsletter(context, newsletter):
+    user = context['request'].user
+    if user.is_authenticated:
+        try:
+            instance = Subscription.objects.get(
+                newsletter=newsletter, user=user
+            )
+            if instance.subscribed:
+                return True
+        except Subscription.DoesNotExist:
+            pass
+    return False
+
+
 def get_newsletter_context(context, next=None, newsletter_slug=None, fallback=True):
     ctx = {
         'next': next,
@@ -45,30 +68,16 @@ def get_newsletter_context(context, next=None, newsletter_slug=None, fallback=Tr
         'has_newsletter': False
     }
 
-    try:
-        newsletter = Newsletter.objects.get(
-            slug=newsletter_slug or settings.DEFAULT_NEWSLETTER
-        )
-    except Newsletter.DoesNotExist:
+    newsletter = _get_newsletter(newsletter_slug)
+    if newsletter is None:
         ctx['has_newsletter'] = True
         return ctx
 
     ctx['newsletter'] = newsletter
     ctx['form'] = NewsletterForm(request=context['request'])
+    ctx['has_newsletter'] = _has_newsletter(context, newsletter)
     user = context['request'].user
-
-    if user.is_authenticated:
-        try:
-            instance = Subscription.objects.get(
-                newsletter=newsletter, user=user
-            )
-            if instance.subscribed:
-                ctx['has_newsletter'] = True
-                return ctx
-        except Subscription.DoesNotExist:
-            pass
-
-        ctx['user'] = user
+    ctx['user'] = user
 
     return ctx
 
@@ -80,6 +89,8 @@ def newsletter_subscribe(context, next=None, newsletter_slug=None, fallback=True
         context, next=next, newsletter_slug=newsletter_slug, fallback=fallback
     )
 
+
 @register.simple_tag(takes_context=True)
-def has_newsletter(context):
-    return get_newsletter_context(context)['has_newsletter']
+def newsletter_is_subscribed(context):
+    newsletter = _get_newsletter()
+    return _has_newsletter(context, newsletter)
