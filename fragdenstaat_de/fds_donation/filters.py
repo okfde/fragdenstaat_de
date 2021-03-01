1142d28b2daeb0757291777d835d826d6ea9142b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-03-01 13:28:48 +0000

    Fix date range filter params passing

--- fragdenstaat_de/fds_donation/filters.py
+++ fragdenstaat_de/fds_donation/filters.py
@@ -49,15 +49,17 @@ class DateRangeFilter(admin.filters.FieldListFilter):
                 value = default_tz.localize(value)
         return value
 
-    def choices(self, cl):
+    def choices(self, changelist):
+        params = changelist.params.copy()
+        for f in self._get_expected_fields():
+            params.pop(f, None)
+
         yield {
             # slugify converts any non-unicode characters to empty characters
             # but system_name is required, if title converts to empty string use id
             # https://github.com/silentsokolov/django-admin-rangefilter/issues/18
             'system_name': str(slugify(self.title) if slugify(self.title) else id(self.title)),
-            'query_string': cl.get_query_string(
-                {}, remove=self._get_expected_fields()
-            )
+            'params': params
         }
 
     def expected_parameters(self):
