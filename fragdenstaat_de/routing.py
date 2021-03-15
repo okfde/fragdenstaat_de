b09abd25474496f9eb9f99807cdc6b21df8922f9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-03-15 21:09:35 +0000

    Add campaign live consumer URL

--- fragdenstaat_de/routing.py
+++ fragdenstaat_de/routing.py
@@ -6,11 +6,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from fragdenstaat_de.fds_cms.consumers import CMSPluginEditConsumer
+from froide_campaign.consumers import CampaignLiveConsumer
 
 from froide.routing import websocket_urls as froide_ws_urls
 
 websocket_urls = froide_ws_urls + [
-    path('fds-cms/edit-plugin/<int:plugin_id>/', CMSPluginEditConsumer.as_asgi())
+    path('fds-cms/edit-plugin/<int:plugin_id>/', CMSPluginEditConsumer.as_asgi()),
+    path('campaign/live/<int:pk>/', CampaignLiveConsumer.as_asgi())
 ]
 
 application = ProtocolTypeRouter({
