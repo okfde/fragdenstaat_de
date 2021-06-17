e90a2e99f84414183bfaf76481cc145f14d1fc27
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-06-17 13:28:08 +0200

    Fix PEP8

--- fragdenstaat_de/fds_mailing/urls.py
+++ fragdenstaat_de/fds_mailing/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('<slug:newsletter_slug>/archiv/<int:year>/<int:month>/<int:day>/<int:pk>/',
         MailingArchiveDetailView.as_view(),
         name='newsletter_archive_detail'),
-    # re_path(r'^(?P<newsletter_slug>[\w-]+)/archiv/$',
-    #     NicerSubmissionArchiveIndexView.as_view(), name='newsletter_archive'
-    # ),
 ]
