724664b9556014b361a09892daf2778fb2c399fe
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-07-12 12:17:48 +0200

    Add newsletter subscriber logic plugins

--- fragdenstaat_de/fds_newsletter/cms_plugins.py
+++ fragdenstaat_de/fds_newsletter/cms_plugins.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
-from .models import NewsletterCMSPlugin
+from .models import Subscriber, NewsletterCMSPlugin
 from .templatetags.newsletter_tags import get_newsletter_context
 
 
@@ -39,3 +40,48 @@ class SmartNewsletterPlugin(CMSPluginBase):
             )
         )
         return context
+
+
+class NewsletterLogicMixin:
+    module = _("Newsletter")
+    cache = False
+    allow_children = True
+    render_template = "fds_newsletter/cms_plugins/newsletter_logic.html"
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        context = self.add_to_context(context)
+        context['should_render'] = self.should_render(context)
+        return context
+
+    def add_to_context(self, context):
+        if not context.get('user') and context.get('request'):
+            if context['request'].user.is_authenticated:
+                context['user'] = context['request'].user
+        if context.get('user') and context['user'].is_authenticated:
+            subscribers = Subscriber.objects.filter(
+                newsletter_slug=settings.DEFAULT_NEWSLETTER,
+                user=context['user']
+            )
+            if subscribers:
+                context['subscriber'] = subscribers[0]
+        return context
+
+    def should_render(self):
+        raise NotImplementedError
+
+
+@plugin_pool.register_plugin
+class IsNewsletterSubscriberPlugin(NewsletterLogicMixin, CMSPluginBase):
+    name = _("Is newsletter subscriber")
+
+    def should_render(self, context):
+        return context.get('subscriber')
+
+
+@plugin_pool.register_plugin
+class IsNotNewsletterSubscriberPlugin(NewsletterLogicMixin, CMSPluginBase):
+    name = _("Is not newsletter subscriber")
+
+    def should_render(self, context):
+        return not context.get('subscriber')
