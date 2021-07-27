473f762ce8b59580d5bee1d16574baa9a485c366
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-07-27 14:22:46 +0200

    Add og image for request page

--- fragdenstaat_de/fds_ogimage/urls.py
+++ fragdenstaat_de/fds_ogimage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.utils.translation import pgettext_lazy
 
-from .views import ProfileOGView
+from .views import ProfileOGView, FoiRequestOGView
 
 
 urlpatterns = [
@@ -9,5 +9,10 @@ urlpatterns = [
         pgettext_lazy("url part", "profile/<str:slug>/_og/"),
         ProfileOGView.as_view(),
         name="account-profile-og",
+    ),
+    path(
+        pgettext_lazy("url part", "request/<str:slug>/_og/"),
+        FoiRequestOGView.as_view(),
+        name="foirequest-show-og",
     ),
 ]
--- fragdenstaat_de/fds_ogimage/views.py
+++ fragdenstaat_de/fds_ogimage/views.py
@@ -1,5 +1,10 @@
 from froide.account.views import ProfileView
+from froide.foirequest.views import FoiRequestView
 
 
 class ProfileOGView(ProfileView):
     template_name = "fds_ogimage/profile.html"
+
+
+class FoiRequestOGView(FoiRequestView):
+    template_name = "fds_ogimage/foirequest.html"
