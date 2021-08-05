a438a353ed536d96a9373242e3af19fc7a706a32
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-08-05 20:35:06 +0200

    Set new mail_domain froide config key

--- fragdenstaat_de/settings/base.py
+++ fragdenstaat_de/settings/base.py
@@ -533,7 +533,8 @@ class FragDenStaatBase(German, Base):
             unsubscribe_enabled=True,
             unsubscribe_format='unsub+{token}@fragdenstaat.de',
             auto_reply_subject_regex=rec('^(Auto-?Reply|Out of office|Out of the office|Abwesenheitsnotiz|Automatische Antwort|automatische Empfangsbestätigung)'),
-            auto_reply_email_regex=rec('^auto(reply|responder|antwort)@')
+            auto_reply_email_regex=rec('^auto(reply|responder|antwort)@'),
+            mail_domain='mail.fragdenstaat.de'
         ))
         return config
 
