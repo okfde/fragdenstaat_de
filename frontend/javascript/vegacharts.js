f239998da878888b3325778f80014bbd30b6debe
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-09-13 12:07:01 +0200

    Make vega charts more configurable

--- frontend/javascript/vegacharts.js
+++ frontend/javascript/vegacharts.js
@@ -2,12 +2,29 @@ import * as vega from 'vega';
 import { expressionInterpreter } from 'vega-interpreter';
 import embed from 'vega-embed';
 
-// Needed so ast: true works and sets expr as vega option
-vega.expressionInterpreter = expressionInterpreter
+class CSPVegaView extends vega.View {
+  constructor (runtime, opt) {
+    opt.expr = expressionInterpreter
+    super(runtime, opt);
+  }
+}
 
 Array.from(document.querySelectorAll('[data-vegachart]')).forEach((el) => {
-  let spec = JSON.parse(el.dataset.vegachart)
+  let spec
+  if (el.dataset.vegachartdata) {
+    spec = JSON.parse(el.dataset.vegachartdata)
+  } else {
+    spec = JSON.parse(document.getElementById(el.dataset.vegachart).textContent);
+  }
+  if (!spec.data.url && !spec.data.values) {
+    let data = JSON.parse(document.getElementById(el.dataset.vegachart + '_data').textContent);
+    spec.data = {"values": data}
+  }
+  // Slightly smaller than container
+  spec.width = Math.floor(el.clientWidth * 4/5)
+
   embed(el, spec, {
-    ast: true // to make CSP compatible
+    viewClass: CSPVegaView,
+    ast: true
   })
 })
