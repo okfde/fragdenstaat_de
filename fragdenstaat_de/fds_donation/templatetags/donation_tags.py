db6dcf65c10721e33b1c58373d12771af1d47b18
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-09-13 12:07:28 +0200

    Add donation stats and charts

---
+++ fragdenstaat_de/fds_donation/templatetags/donation_tags.py
@@ -0,0 +1,49 @@
+from django import template
+from django.db.models import Q, Max, Count, Sum
+from django.db.models.functions import TruncMonth
+from django.utils import timezone
+
+from dateutil.relativedelta import relativedelta
+
+from ..models import Donor, Donation
+
+
+register = template.Library()
+
+ACTIVE_TIME = relativedelta(months=24)
+SLEEPING_TIME = relativedelta(months=36)
+
+
+@register.inclusion_tag('fds_donation/stats/admin_dashboard.html', takes_context=True)
+def donor_type_counts(context):
+    now = timezone.now()
+
+    active_filter = Q(last_donation__gte=now - ACTIVE_TIME)
+    sleeping_filter = Q(last_donation__lt=now - ACTIVE_TIME) & Q(last_donation__gte=now - SLEEPING_TIME)
+    inactive_filter = Q(last_donation__lt=now - SLEEPING_TIME)
+
+    aggs = Donor.objects.annotate(
+        last_donation=Max('donations__timestamp', filter=Q(donations__received=True))
+    ).aggregate(
+        active_count=Count('id', filter=active_filter),
+        sleeping_count=Count('id', filter=sleeping_filter),
+        inactive_count=Count('id', filter=inactive_filter),
+    )
+    return {
+        'aggs': aggs
+    }
+
+
+@register.inclusion_tag('fds_donation/stats/donation_chart.html', takes_context=True)
+def donations_by_month(context):
+    donations_by_month = Donation.objects.order_by().filter(
+        received_timestamp__isnull=False
+    ).annotate(
+        month=TruncMonth('timestamp')
+    ).values('month', 'recurring').annotate(
+        amount_by_month=Sum('amount')
+    ).order_by('month', 'recurring')
+
+    return {
+        'chart_data': list(donations_by_month)
+    }
