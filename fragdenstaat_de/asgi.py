57c673b9cc0afb91b50ad5a0de0fb9391fe609c9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-01-17 13:00:29 +0000

    Add Sentry ASGI middleware

--- fragdenstaat_de/asgi.py
+++ fragdenstaat_de/asgi.py
@@ -7,6 +7,11 @@ import os
 import django
 from channels.routing import get_default_application
 
+try:
+    from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+except ImportError:
+    SentryAsgiMiddleware = None
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fragdenstaat_de.settings.development")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
@@ -15,4 +20,8 @@ from configurations import importer  # noqa
 importer.install()
 
 django.setup()
+
 application = get_default_application()
+
+if SentryAsgiMiddleware:
+    application = SentryAsgiMiddleware(application)
