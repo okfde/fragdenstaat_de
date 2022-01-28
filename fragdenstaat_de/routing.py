70658871b4954dfac5c1d5e386499f71d4931116
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-01-28 10:20:33 +0000

    Add lifespan app to routing as workaround
    This is to avoid "No application configured for scope type 'lifespan'" ValueErrors

--- fragdenstaat_de/routing.py
+++ fragdenstaat_de/routing.py
@@ -15,9 +15,32 @@ websocket_urls = froide_ws_urls + [
     path("campaign/live/<int:pk>/", CampaignLiveConsumer.as_asgi()),
 ]
 
+
+class LifespanApp:
+    """
+    temporary shim for https://github.com/django/channels/issues/1216
+    needed so that hypercorn doesn't display an error.
+    this uses ASGI 2.0 format, not the newer 3.0 single callable
+    """
+
+    def __init__(self, scope):
+        self.scope = scope
+
+    async def __call__(self, receive, send):
+        if self.scope["type"] == "lifespan":
+            while True:
+                message = await receive()
+                if message["type"] == "lifespan.startup":
+                    await send({"type": "lifespan.startup.complete"})
+                elif message["type"] == "lifespan.shutdown":
+                    await send({"type": "lifespan.shutdown.complete"})
+                    return
+
+
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "lifespan": LifespanApp,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter([path("ws/", URLRouter(websocket_urls))]))
         ),
