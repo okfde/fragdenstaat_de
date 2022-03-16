f5ec370d35cc525d438de9c1cfab9515c3b5102a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-03-16 10:30:29 +0100

    Allow leaving out path for og image
    CMS app views are wrapped in perms decorator, can't access template.
    So need to give template explicitly.

--- fragdenstaat_de/fds_ogimage/templatetags/fds_ogimage.py
+++ fragdenstaat_de/fds_ogimage/templatetags/fds_ogimage.py
@@ -10,18 +10,20 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def ogimage_url(context, path=None):
-    try:
-        match = resolve(path)
-    except Resolver404:
-        return ""
-    view_class = getattr(match.func, "view_class", None)
-    if view_class is None:
-        return ""
-    template_name = getattr(view_class, "template_name", None)
-    if not template_name:
-        return ""
-    rendered = render_to_string(template_name, context.flatten())
+def ogimage_url(context, path=None, template=None):
+    if template is None:
+        try:
+            match = resolve(path)
+        except Resolver404:
+            return ""
+        view_class = getattr(match.func, "view_class", None)
+        if view_class is None:
+            return ""
+        template = getattr(view_class, "template_name", None)
+        if not template:
+            return ""
+
+    rendered = render_to_string(template, context.flatten())
     h = hashlib.sha256()
     h.update(rendered.encode("utf-8"))
     hex_digest = h.hexdigest()
