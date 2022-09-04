b49ec06c9cd45dc475a7a975564ab61d93787b63
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-09-04 13:04:31 +0200

    Upgrade sentry, add console error capturing

--- frontend/javascript/sentry.ts
+++ frontend/javascript/sentry.ts
@@ -1,10 +1,16 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations as TracingIntegrations } from '@sentry/tracing'
+import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations'
 
 export function init(dsn: string): void {
   Sentry.init({
     dsn: dsn,
-    integrations: [new TracingIntegrations.BrowserTracing()],
+    integrations: [
+      new TracingIntegrations.BrowserTracing(),
+      new CaptureConsoleIntegration({
+        levels: ['error']
+      })
+    ],
     tracesSampleRate: 1.0
   })
 }
