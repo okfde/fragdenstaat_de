55e217a862076ead5d0e62acf72fbd33927ac15e
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-09-28 18:17:26 +0200

    Add more models and plugins

--- fragdenstaat_de/fds_abtesting/models.py
+++ fragdenstaat_de/fds_abtesting/models.py
@@ -1,6 +1,6 @@
-from unicodedata import name
-
 from django.db import models
+
+from cms.models.pluginmodel import CMSPlugin
 
 
 class ABTest(models.Model):
@@ -8,4 +8,19 @@ class ABTest(models.Model):
     action = models.CharField(max_length=500)
 
     def __str__(self):
-        return name
+        return self.name
+
+
+class ABTestEvent(models.Model):
+    ab_test = models.ForeignKey(ABTest, on_delete=models.CASCADE)
+    variant = models.CharField(max_length=500)
+
+
+class ABTestVariant(CMSPlugin):
+    name = models.CharField(max_length=500)
+    action_text = models.CharField(max_length=500)
+    action_class = models.CharField(max_length=500, blank=True)
+
+
+class ABTestCMSPlugin(CMSPlugin):
+    ab_test = models.ForeignKey(ABTest, on_delete=models.CASCADE)
