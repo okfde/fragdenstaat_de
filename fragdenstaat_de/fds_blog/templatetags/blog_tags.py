af414f8c71b4297361971da1829402bd2cf30f50
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-12-13 11:20:38 +0100

    Match next read with current articles language

--- fragdenstaat_de/fds_blog/templatetags/blog_tags.py
+++ fragdenstaat_de/fds_blog/templatetags/blog_tags.py
@@ -8,8 +8,7 @@ register = template.Library()
 
 @register.filter
 def get_next_read(article):
-
-    qs = articles_published(Article.objects.all())
+    qs = articles_published(Article.objects.filter(language=article.language))
     if article.start_publication:
         return qs.filter(start_publication__lt=article.start_publication).first()
     return qs.exclude(pk=article.pk).first()
