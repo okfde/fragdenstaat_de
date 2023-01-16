d2f5f21b4fd0217e356c5e78961adec8a7a0fcc3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2023-01-16 14:11:43 +0100

    Revert "Upgrade to BS 5.3-alpha1, upgrade other frontend dependencies"
    This reverts commit 4c0c47e34bd46fdca4bffa77c48eb8491e3b295a.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import autoprefixer from 'autoprefixer'
 import devManifest from 'vite-plugin-dev-manifest'
+import autoprefixer from 'autoprefixer'
 import vue from '@vitejs/plugin-vue2'
 
 const outputDir = resolve(__dirname, 'build')
