30c9892452db7d47a1829a01b08895b76386c414
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2023-06-28 10:34:20 +0200

    Add search keyboard shortcut Ctrl+K

--- frontend/javascript/misc.ts
+++ frontend/javascript/misc.ts
@@ -1,5 +1,6 @@
 import './misc/highlight-anchor'
 import './misc/iframe-height'
+import './misc/keyboard-shortcuts'
 import './misc/matomo'
 import './misc/reference-forms'
 import './misc/reveal-more'
