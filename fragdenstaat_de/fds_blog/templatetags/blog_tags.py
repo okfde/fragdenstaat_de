b6b271de895ce237c6e2c71943bb43c8925f98a8
krmax44 <hi@krmax44.de>
2023-10-02 16:08:28 +0200

    ✨ add blog preview tag

--- fragdenstaat_de/fds_blog/templatetags/blog_tags.py
+++ fragdenstaat_de/fds_blog/templatetags/blog_tags.py
@@ -12,3 +12,11 @@ def get_next_read(article):
     if article.start_publication:
         return qs.filter(start_publication__lt=article.start_publication).first()
     return qs.exclude(pk=article.pk).first()
+
+
+@register.inclusion_tag("fds_blog/blog_preview.html", takes_context=True)
+def get_blog_preview(context, amount=6):
+    amount = min(int(amount), 20)
+    articles = Article.published.all()[:amount]
+
+    return {"articles": articles, "request": context.get("request")}
