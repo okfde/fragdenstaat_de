d7478733ebab610f4aef1bd58c2a7b9e3656b5db
gpkvt <git@21x9.org>
2023-12-15 11:20:48 +0100

    Generate static sitemaps

--- fragdenstaat_de/theme/management/commands/render_sitemap.py
+++ fragdenstaat_de/theme/management/commands/render_sitemap.py
@@ -9,17 +9,33 @@ class Command(BaseCommand):
     help = "Render sitemap"
 
     def add_arguments(self, parser):
-        parser.add_argument("section", help="Render section", default="")
+        parser.add_argument(
+            "--section", help="Render section", default="", required=False
+        )
+        parser.add_argument(
+            "--getsections",
+            help="List available sections",
+            required=False,
+            action="store_true",
+        )
 
     def handle(self, *args, **options):
-        section = options["section"]
-        factory = RequestFactory()
-        if section:
-            request = factory.get("/sitemap-{}.xml".format(section))
-            response = sitemaps_views.sitemap(request, sitemaps)
+        if options["getsections"]:
+            for section in sitemaps:
+                self.stdout.write(section)
         else:
-            request = factory.get("/sitemap.xml")
-            response = sitemaps_views.index(
-                request, sitemaps, sitemap_url_name="sitemaps"
-            )
-        self.stdout.write(response.rendered_content)
+            section = options["section"]
+            factory = RequestFactory()
+            if section:
+                sitemap_name = "/sitemap-{}.xml".format(section)
+                request = factory.get(sitemap_name)
+                response = sitemaps_views.sitemap(request, sitemaps)
+            else:
+                sitemap_name = "/sitemap.xml"
+                request = factory.get(sitemap_name)
+                response = sitemaps_views.index(
+                    request, sitemaps, sitemap_url_name="sitemaps"
+                )
+            sitemap_dest_tmp = "/tmp/{}".format(sitemap_name)
+            with open(sitemap_dest_tmp, "w") as sitemap_out:
+                sitemap_out.write(response.rendered_content)
