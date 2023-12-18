9ce696c3d4e40aa6ebf09d1ef069dc87140b345e
gpkvt <git@21x9.org>
2023-12-18 13:27:29 +0100

    Generate all sitemaps if no section is given, added outdir option

--- fragdenstaat_de/theme/management/commands/render_sitemap.py
+++ fragdenstaat_de/theme/management/commands/render_sitemap.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.sitemaps import views as sitemaps_views
 from django.core.management.base import BaseCommand
 from django.test import RequestFactory
@@ -18,24 +20,68 @@ class Command(BaseCommand):
             required=False,
             action="store_true",
         )
+        parser.add_argument(
+            "--outdir", help="Output directory", required=False, default="/tmp/"
+        )
+
+    def get_sections(self):
+        sections = []
+        for section in sitemaps:
+            sections.append(section)
+        return sections
+
+    def write_sitemap_tempfile(self, sitemap_file, sitemap_content):
+        with open(sitemap_file, "w") as sitemap_out:
+            sitemap_out.write(sitemap_content)
+        return True
 
     def handle(self, *args, **options):
+        sections = []
+        sections = self.get_sections()
+        section = options["section"]
+        outdir = options["outdir"]
+
         if options["getsections"]:
-            for section in sitemaps:
-                self.stdout.write(section)
-        else:
-            section = options["section"]
-            factory = RequestFactory()
-            if section:
-                sitemap_name = "/sitemap-{}.xml".format(section)
-                request = factory.get(sitemap_name)
-                response = sitemaps_views.sitemap(request, sitemaps)
-            else:
-                sitemap_name = "/sitemap.xml"
-                request = factory.get(sitemap_name)
-                response = sitemaps_views.index(
-                    request, sitemaps, sitemap_url_name="sitemaps"
+            for s in sections:
+                self.stdout.write(s)
+            exit()
+
+        if section and section not in sections:
+            self.stderr.write(
+                "Error: Section {} does not exists. Use '--getsections' for a valid list.".format(
+                    section
                 )
-            sitemap_dest_tmp = "/tmp/{}".format(sitemap_name)
-            with open(sitemap_dest_tmp, "w") as sitemap_out:
-                sitemap_out.write(response.rendered_content)
+            )
+            exit()
+
+        if not os.path.isdir(outdir):
+            self.stderr.write(
+                "Error: The directory {} does not exists, please check/create and try again.".format(
+                    outdir
+                )
+            )
+            exit()
+
+        self.stdout.write("Generating sitemap(s), this might take a while...")
+
+        if section:
+            sections.clear()
+            sections = [section]
+
+        for s in sections:
+            self.stdout.write("{}".format(s))
+            factory = RequestFactory()
+            sitemap_name = "/sitemap-{}.xml".format(s)
+            request = factory.get(sitemap_name)
+            response = sitemaps_views.sitemap(request, sitemaps, section=s)
+
+            sitemap_dest_tmp = "{}/{}".format(outdir, sitemap_name)
+            self.write_sitemap_tempfile(sitemap_dest_tmp, response.rendered_content)
+
+        sitemap_name = "/sitemap.xml"
+        request = factory.get(sitemap_name)
+        response = sitemaps_views.index(request, sitemaps, sitemap_url_name="sitemaps")
+        sitemap_dest_tmp = "{}/{}".format(outdir, sitemap_name)
+        self.write_sitemap_tempfile(sitemap_dest_tmp, response.rendered_content)
+
+        self.stdout.write("Done")
