02ba5c4ad5ffcdeb1e8a7b0d744e59c7d7fca825
krmax44 <hi@krmax44.de>
2024-04-19 16:47:07 +0200

    ✨ add blog toolbar to all blog pages

--- fragdenstaat_de/fds_blog/cms_toolbars.py
+++ fragdenstaat_de/fds_blog/cms_toolbars.py
@@ -8,13 +8,18 @@ from cms.toolbar_pool import toolbar_pool
 
 class BlogToolbar(CMSToolbar):
     def populate(self):
-        if not hasattr(self.request, "article"):
-            return
-
         blog_menu = self.toolbar.get_or_create_menu("blog-menu", _("Blog"))
-        article = self.request.article
-        url = reverse("admin:fds_blog_article_change", args=(article.pk,))
-        blog_menu.add_modal_item(_("Edit article"), url=url)
+
+        if hasattr(self.request, "article"):
+            article = self.request.article
+            url = reverse("admin:fds_blog_article_change", args=(article.pk,))
+            blog_menu.add_modal_item(_("Edit article"), url=url)
+        else:
+            url = reverse("admin:fds_blog_article_changelist")
+            blog_menu.add_modal_item(_("Edit articles"), url=url)
+
+        url = reverse("admin:fds_blog_article_add")
+        blog_menu.add_modal_item(_("Create new article"), url=url)
 
 
 toolbar_pool.register(BlogToolbar)
