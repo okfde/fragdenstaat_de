edb2435970247ce3158b053ddda4cfa17d4da7dc
krmax44 <hi@krmax44.de>
2024-05-27 10:51:35 +0200

    🐛 fix navbar closing on mobile when using search

--- frontend/javascript/navbar.ts
+++ frontend/javascript/navbar.ts
@@ -4,6 +4,8 @@ const header = document.querySelector('#header')
 let open: HTMLElement | undefined = undefined
 let hide: () => void = () => {}
 
+const navSearch = header?.querySelector<HTMLElement>('.nav-search')
+
 const menuToggles = [
   ...(header?.querySelectorAll<HTMLElement>('.nav-toggle-menu') ?? [])
 ]
@@ -52,18 +54,23 @@ menuToggles.forEach((el) =>
   })
 )
 
-window.addEventListener('resize', () => hide())
+window.addEventListener('resize', () => {
+  // hide navbar when window resizes, except when the search is focused
+  // (resize due to onscreen keyboard on mobile devices)
+  if (navSearch?.contains(document.activeElement) !== true) hide()
+})
 
 window.addEventListener('click', (e) => {
   if (
     open !== undefined &&
-    header?.contains(e.target as HTMLElement) === false
+    header?.contains(e.target as HTMLElement) === false // click is outside of navbar
   ) {
     hide()
   }
 })
 
 window.addEventListener('scroll', () => {
+  // hide navbar when scrolling down
   if (open && open.clientHeight * 2 < window.scrollY) {
     hide()
   }
@@ -103,7 +110,7 @@ function updateDropdowns(): void {
 window.addEventListener('resize', updateDropdowns)
 updateDropdowns()
 
-const navSearch = header?.querySelector<HTMLElement>('.nav-search')
+// hide search in navbar if the current page is a search page
 const searchUrls = [
   ...(navSearch?.querySelector<HTMLSelectElement>('select') ?? [])
 ].map((el) => el.value)
@@ -113,11 +120,11 @@ if (searchUrls.includes(window.location.pathname)) {
   navSearch?.remove()
 }
 
+// keyboard shortcuts for search
 const input = navSearch?.querySelector('input')
 const placeholder = input?.getAttribute('placeholder') ?? ''
 const isMac = navigator.userAgent.includes('Mac OS X')
 if (isMac) {
-  // macOS
   input?.setAttribute('placeholder', `${placeholder} (⌘ + K)`)
 } else {
   input?.setAttribute('placeholder', `${placeholder} (Ctrl + K)`)
