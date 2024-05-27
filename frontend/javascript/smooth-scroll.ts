9bf2326023b590512f9e851366edc3ce542b74ae
krmax44 <hi@krmax44.de>
2024-05-27 11:49:48 +0200

    🐛 don't prevent external links from opening with `.smooth-scroll`

--- frontend/javascript/smooth-scroll.ts
+++ frontend/javascript/smooth-scroll.ts
@@ -2,6 +2,12 @@ const anchors: NodeListOf<HTMLAnchorElement> =
   document.querySelectorAll('a.smooth-scroll')
 anchors.forEach((element) => {
   element.addEventListener('click', (event) => {
+    const a = event.target as HTMLAnchorElement
+    if (
+      a.host !== window.location.host ||
+      a.pathname !== window.location.pathname
+    )
+      return
     event.preventDefault()
 
     const { hash, href } = element
