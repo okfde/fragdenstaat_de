00501668894a1b8816fd53f79e81196094f13865
krmax44 <hi@krmax44.de>
2024-06-03 19:43:36 +0200

    🐛 fix blog urls precedence

--- fragdenstaat_de/fds_blog/urls.py
+++ fragdenstaat_de/fds_blog/urls.py
@@ -23,33 +23,18 @@ REDIRECT_PATTERNS = [
     "<int:year>/<int:month>/<int:day>/<slug:slug>/",
     "<int:year>/<int:month>/<slug:slug>/",
     "<slug:category>/<slug:slug>/",
-    "<int:pk>/",
 ]
 
 redirect_urls = [
     path(urlconf, ArticleRedirectView.as_view()) for urlconf in REDIRECT_PATTERNS
 ]
 
-# redirect urls need to come before actual urls: with /1970/01/01/foo/,
-# "1970" could also be a category slug.
-
+# the order is important!
+# /1970/01/ could be /<slug:category>/<slug:slug>/ (article page),
+# but archive at /<int:year>/<int:month>/ (with stricter url params) should have higher priority
 urlpatterns = (
-    [path("", ArticleListView.as_view(), name="article-latest")]
-    + redirect_urls
-    + [
-        path(
-            "<slug:category>/<int:year>/<int:month>/<slug:slug>/",
-            ArticleDetailView.as_view(),
-            name="article-detail",
-        ),
-        path(_("search/"), ArticleSearchView.as_view(), name="article-search"),
-        path("feed/", LatestArticlesFeed(), name="article-latest-feed"),
-        path("feed/audio/", LatestAudioFeed(), name="article-latest-feed-audio"),
-        path(
-            "feed/teaser/",
-            LatestArticlesTeaserFeed(),
-            name="article-latest-feed-teaser",
-        ),
+    [
+        path("", ArticleListView.as_view(), name="article-latest"),
         path("<int:year>/", ArticleArchiveView.as_view(), name="article-archive"),
         path(
             "<int:year>/<int:month>/",
@@ -66,13 +51,31 @@ urlpatterns = (
             CategoryArticleRedirectView.as_view(),
             name="article-category-redirect",
         ),
+        path("tag/<slug:tag>/", TaggedListView.as_view(), name="article-tagged"),
+        path(_("search/"), ArticleSearchView.as_view(), name="article-search"),
+        path("feed/", LatestArticlesFeed(), name="article-latest-feed"),
+        path("feed/audio/", LatestAudioFeed(), name="article-latest-feed-audio"),
+        path(
+            "feed/teaser/",
+            LatestArticlesTeaserFeed(),
+            name="article-latest-feed-teaser",
+        ),
+    ]
+    + redirect_urls
+    + [
+        path(
+            "<slug:category>/<int:year>/<int:month>/<slug:slug>/",
+            ArticleDetailView.as_view(),
+            name="article-detail",
+        ),
         path(
             "<slug:category>/",
             root_slug_view,
             name="article-category",
         ),
-        path("tag/<slug:tag>/", TaggedListView.as_view(), name="article-tagged"),
     ]
 )
+
+SHORT_ARTICLE_URL = path("p/", ArticleRedirectView.as_view(), name="article-short-url")
 
 api_router.register(r"articletag", ArticleTagViewSet, basename="articletag")
