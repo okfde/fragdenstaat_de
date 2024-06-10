5e4a517f580a079040f8a22cba25d158561aac5b
krmax44 <hi@krmax44.de>
2024-06-10 18:34:32 +0200

    🏗️ finalize url redirects

---
+++ fragdenstaat_de/fds_blog/redirect_urls.py
@@ -0,0 +1,76 @@
+from django.shortcuts import redirect
+from django.urls import path, re_path
+from django.utils.translation import gettext_lazy as _
+
+from .redirect_views import (
+    ArchiveRedirectView,
+    AuthorRedirectView,
+    CategoryRedirectView,
+    SearchRedirectView,
+    TagRedirectView,
+)
+from .views import ArticleRedirectView
+
+
+def fixed_redirect(pattern_name):
+    return lambda request: redirect(pattern_name, permanent=True)
+
+
+ARTICLE_URLS = [
+    r"^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>\w[-\w]*)/$",
+    r"^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>\w[-\w]*)/$",
+    r"^(?P<category>\w[-\w]*)/(?P<slug>\w[-\w]*)/$",
+    r"^(?P<slug>\w[-\w]*)/$",
+]
+
+urlpatterns = [
+    path("", fixed_redirect("blog:article-latest"), name="legacy-article-latest"),
+    re_path(
+        _(r"^search/$"),
+        SearchRedirectView.as_view(),
+        name="legacy-article-search",
+    ),
+    re_path(
+        r"^feed/$",
+        fixed_redirect("blog:article-latest-feed"),
+        name="legacy-article-latest-feed",
+    ),
+    re_path(
+        r"^feed/audio/$",
+        fixed_redirect("blog:article-latest-feed-audio"),
+        name="legacy-article-latest-feed-audio",
+    ),
+    re_path(
+        r"^feed/teaser/$",
+        fixed_redirect("blog:article-latest-feed-teaser"),
+        name="legacy-article-latest-feed-teaser",
+    ),
+    re_path(
+        r"^(?P<year>\d{4})/$",
+        ArchiveRedirectView.as_view(),
+        name="legacy-article-archive",
+    ),
+    re_path(
+        r"^(?P<year>\d{4})/(?P<month>\d{1,2})/$",
+        ArchiveRedirectView.as_view(),
+        name="legacy-article-archive",
+    ),
+    re_path(
+        _(r"^author/(?P<username>[\w\.@+-]+)/$"),
+        AuthorRedirectView.as_view(),
+        name="legacy-article-author",
+    ),
+    re_path(
+        _(r"^category/(?P<category>[\w\.@+-]+)/$"),
+        CategoryRedirectView.as_view(),
+        name="legacy-article-category",
+    ),
+    re_path(
+        r"^tag/(?P<tag>[-\w]+)/$",
+        TagRedirectView.as_view(),
+        name="legacy-article-tagged",
+    ),
+] + [
+    re_path(urlconf, ArticleRedirectView.as_view(), name="legacy-article-redirect")
+    for urlconf in ARTICLE_URLS
+]
--- fragdenstaat_de/fds_blog/urls.py
+++ fragdenstaat_de/fds_blog/urls.py
@@ -9,73 +9,50 @@ from .views import (
     ArticleArchiveView,
     ArticleDetailView,
     ArticleListView,
-    ArticleRedirectView,
     ArticleSearchView,
     AuthorArticleView,
-    CategoryArticleRedirectView,
     TaggedListView,
     root_slug_view,
 )
 
 app_name = "blog"
 
-REDIRECT_PATTERNS = [
-    "<int:year>/<int:month>/<int:day>/<slug:slug>/",
-    "<int:year>/<int:month>/<slug:slug>/",
-    "<slug:category>/<slug:slug>/",
-]
-
-redirect_urls = [
-    path(urlconf, ArticleRedirectView.as_view()) for urlconf in REDIRECT_PATTERNS
-]
-
 # the order is important!
 # /1970/01/ could be /<slug:category>/<slug:slug>/ (article page),
 # but archive at /<int:year>/<int:month>/ (with stricter url params) should have higher priority
-urlpatterns = (
-    [
-        path("", ArticleListView.as_view(), name="article-latest"),
-        path("<int:year>/", ArticleArchiveView.as_view(), name="article-archive"),
-        path(
-            "<int:year>/<int:month>/",
-            ArticleArchiveView.as_view(),
-            name="article-archive",
-        ),
-        path(
-            _("author/<str:username>/"),
-            AuthorArticleView.as_view(),
-            name="article-author",
-        ),
-        path(
-            _("category/<slug:category>/"),
-            CategoryArticleRedirectView.as_view(),
-            name="article-category-redirect",
-        ),
-        path("tag/<slug:tag>/", TaggedListView.as_view(), name="article-tagged"),
-        path(_("search/"), ArticleSearchView.as_view(), name="article-search"),
-        path("feed/", LatestArticlesFeed(), name="article-latest-feed"),
-        path("feed/audio/", LatestAudioFeed(), name="article-latest-feed-audio"),
-        path(
-            "feed/teaser/",
-            LatestArticlesTeaserFeed(),
-            name="article-latest-feed-teaser",
-        ),
-    ]
-    + redirect_urls
-    + [
-        path(
-            "<slug:category>/<int:year>/<int:month>/<slug:slug>/",
-            ArticleDetailView.as_view(),
-            name="article-detail",
-        ),
-        path(
-            "<slug:category>/",
-            root_slug_view,
-            name="article-category",
-        ),
-    ]
-)
-
-SHORT_ARTICLE_URL = path("p/", ArticleRedirectView.as_view(), name="article-short-url")
+urlpatterns = [
+    path("", ArticleListView.as_view(), name="article-latest"),
+    path("<int:year>/", ArticleArchiveView.as_view(), name="article-archive"),
+    path(
+        "<int:year>/<int:month>/",
+        ArticleArchiveView.as_view(),
+        name="article-archive",
+    ),
+    path(
+        _("author/<str:username>/"),
+        AuthorArticleView.as_view(),
+        name="article-author",
+    ),
+    path("tag/<slug:tag>/", TaggedListView.as_view(), name="article-tagged"),
+    path(_("search/"), ArticleSearchView.as_view(), name="article-search"),
+    path("feed/", LatestArticlesFeed(), name="article-latest-feed"),
+    path("feed/audio/", LatestAudioFeed(), name="article-latest-feed-audio"),
+    path(
+        "feed/teaser/",
+        LatestArticlesTeaserFeed(),
+        name="article-latest-feed-teaser",
+    ),
+] + [
+    path(
+        "<slug:category>/<int:year>/<int:month>/<slug:slug>/",
+        ArticleDetailView.as_view(),
+        name="article-detail",
+    ),
+    path(
+        "<slug:category>/",
+        root_slug_view,
+        name="article-category",
+    ),
+]
 
 api_router.register(r"articletag", ArticleTagViewSet, basename="articletag")
