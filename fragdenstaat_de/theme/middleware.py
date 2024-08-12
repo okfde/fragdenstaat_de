2041c13ef6e03f25d0b21331c2a9b5ecc62afd8a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2024-08-12 09:55:52 +0100

    Allow frame ancestor URLs per CMS page
    Valid Frame ancestor URLs are stored in the FDS page extension and are validated on save to be a space-separated list of URLs.
    The CSP header is then set in the middleware that also disables X-Frame-Options selectively.

--- fragdenstaat_de/theme/middleware.py
+++ fragdenstaat_de/theme/middleware.py
@@ -4,6 +4,16 @@ from django.middleware.clickjacking import XFrameOptionsMiddleware
 class XFrameOptionsCSPMiddleware(XFrameOptionsMiddleware):
     def process_response(self, request, response):
         response = super().process_response(request, response)
+        current_page = getattr(request, "current_page", None)
+        if current_page is not None:
+            # current_page is a lazy object that only evaluates to None on attr access
+            extension = getattr(current_page, "fdspageextension", None)
+            if extension is not None and extension.frame_ancestors:
+                frame_ancestor_urls = " ".join(extension.get_frame_ancestors())
+                response.headers["Content-Security-Policy"] = (
+                    "frame-ancestors 'self' %s" % frame_ancestor_urls
+                )
+                del response["X-Frame-Options"]
         if response.has_header("X-Frame-Options"):
             response.headers["Content-Security-Policy"] = "frame-ancestors 'self'"
         return response
