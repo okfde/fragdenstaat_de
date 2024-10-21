8f423edeb7c0ac077c84abe7c81a96f2268d3028
krmax44 <hi@krmax44.de>
2024-10-21 12:32:28 +0200

    ✨ improve paperless document import

--- fragdenstaat_de/fds_paperless/forms.py
+++ fragdenstaat_de/fds_paperless/forms.py
@@ -1,44 +1,27 @@
-import re
-from datetime import date
-
 from django import forms
 from django.core.files.base import ContentFile
+from django.utils.translation import gettext_lazy as _
 
 from froide.foirequest.forms.message import PostalReplyForm
-from froide.helper.widgets import BootstrapCheckboxSelectMultiple
 
 from .paperless import get_document_data
 
 
 class PaperlessPostalReplyForm(PostalReplyForm):
     files = None
-    paperless_ids = forms.MultipleChoiceField(
-        label="Paperless Dokumente",
-        widget=BootstrapCheckboxSelectMultiple,
+    paperless_ids = forms.TypedMultipleChoiceField(
+        label=_("Paperless Documents"),
         required=True,
+        widget=forms.MultipleHiddenInput,
+        coerce=int,
     )
 
     def __init__(self, *args, **kwargs):
         paperless_docs = kwargs.pop("paperless_docs")
         super().__init__(*args, **kwargs)
         self.fields["paperless_ids"].choices = [
-            (
-                doc["id"],
-                "{date}: ({title}) - {content}...".format(
-                    date=doc["added"],
-                    title=doc["title"],
-                    content=doc["content"][:100],
-                ),
-            )
-            for doc in paperless_docs
+            (doc["id"], doc["id"]) for doc in paperless_docs
         ]
-        if paperless_docs:
-            DATE_RE = re.compile(r"(\d{1,2})\.(\d{1,2})\.(\d{4})")
-            match = DATE_RE.search(paperless_docs[0]["content"])
-            if match:
-                self.fields["date"].initial = date(
-                    int(match.group(3)), int(match.group(2)), int(match.group(1))
-                )
 
     def clean(self):
         # Do not enforce presence of either files or text
--- fragdenstaat_de/fds_paperless/paperless.py
+++ fragdenstaat_de/fds_paperless/paperless.py
@@ -1,12 +1,15 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
-from django.conf import settings
+from django.conf import ImproperlyConfigured, settings
 from django.utils import timezone
 
 import requests
 
 
 def get_paperless_client():
+    if not settings.PAPERLESS_API_URL:
+        raise ImproperlyConfigured("PAPERLESS_API_URL not set")
+
     session = requests.Session()
     session.headers.update({"Authorization": settings.PAPERLESS_API_TOKEN})
     return session
@@ -30,8 +33,47 @@ def get_document_data(paperless_document_id):
 def list_documents():
     client = get_paperless_client()
     last_week = timezone.now() - timedelta(days=7)
-    API_URL = settings.PAPERLESS_API_URL + "/documents/?added__date__gt={}".format(
-        last_week.date()
+    API_URL = (
+        settings.PAPERLESS_API_URL
+        + "/documents/?added__date__gt={}&document_type__isnull=1".format(
+            last_week.date()
+        )
     )
+
     data = client.get(API_URL).json()
-    return list(reversed(data["results"]))
+
+    def map_doc(document):
+        document["created"] = datetime.fromisoformat(document["created"])
+        document["url"] = get_preview_link(document["id"])
+        return document
+
+    data = [map_doc(doc) for doc in data["results"]]
+
+    return list(reversed(data))
+
+
+def get_preview_link(paperless_document_id):
+    return settings.PAPERLESS_API_URL + f"/documents/{paperless_document_id}/preview/"
+
+
+def get_thumbnail(paperless_document_id: int) -> tuple[str, bytes]:
+    client = get_paperless_client()
+    THUMBNAIL_URL = (
+        settings.PAPERLESS_API_URL + f"/documents/{paperless_document_id}/thumb/"
+    )
+    thumbnail = client.get(THUMBNAIL_URL)
+    return thumbnail.headers.get("Content-Type", ""), thumbnail.content
+
+
+def add_tag_to_documents(paperless_document_ids: list[int]):
+    client = get_paperless_client()
+
+    API_URL = settings.PAPERLESS_API_URL + "/documents/bulk_edit/"
+
+    data = {
+        "documents": paperless_document_ids,
+        "method": "set_document_type",
+        "parameters": {"document_type": settings.PAPERLESS_UPLOADED_TYPE},
+    }
+
+    client.post(API_URL, json=data)
