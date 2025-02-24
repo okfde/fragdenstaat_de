d52f49d3168d06f82c64f9fc5f1c40bbc2572e74
krmax44 <hi@krmax44.de>
2025-02-24 13:31:54 +0100

    🥅 newsletter import: show error message when no email template is found

--- fragdenstaat_de/fds_newsletter/forms.py
+++ fragdenstaat_de/fds_newsletter/forms.py
@@ -2,11 +2,13 @@ from io import StringIO
 
 from django import forms
 from django.conf import settings
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from froide.helper.spam import SpamProtectionMixin
 from froide.helper.widgets import BootstrapCheckboxSelectMultiple, BootstrapRadioSelect
+
+from fragdenstaat_de.fds_mailing.models import EmailTemplate
 
 from .models import Newsletter, Subscriber, UnsubscribeFeedback
 from .utils import import_csv, subscribe, subscribed_newsletters
@@ -153,6 +155,24 @@ class SubscriberImportForm(forms.Form):
     email_confirmed = forms.BooleanField(
         label=_("Email addresses are confirmed"), required=False
     )
+
+    def clean_email_confirmed(self):
+        email_confirmed = self.cleaned_data["email_confirmed"]
+
+        if not email_confirmed:
+            try:
+                EmailTemplate.objects.get(
+                    mail_intent="fds_newsletter/email/subscriber_batch_confirm",
+                    active=True,
+                )
+            except (EmailTemplate.DoesNotExist, EmailTemplate.MultipleObjectsReturned):
+                raise ValidationError(
+                    _(
+                        "Make sure there exists one active mailing template for the batch confirm intent (fds_newsletter/email/subscriber_batch_confirm)."
+                    )
+                ) from None
+
+        return email_confirmed
 
     def save(self, newsletter):
         csv_file = self.cleaned_data["csv_file"]
