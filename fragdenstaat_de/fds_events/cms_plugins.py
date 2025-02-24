c82408be485a7529a09d71d43a97de91a99f3f77
krmax44 <hi@krmax44.de>
2025-02-24 15:41:26 +0100

    ✨ add event calendar app

---
+++ fragdenstaat_de/fds_events/cms_plugins.py
@@ -0,0 +1,35 @@
+import datetime
+
+from django.utils.translation import gettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from froide_legalaction.models import Lawsuit
+
+from .models import Event
+
+
+def sort_event(event: Event | Lawsuit) -> datetime.date:
+    if isinstance(event.start_date, datetime.datetime):
+        return event.start_date.date()
+    return event.start_date
+
+
+@plugin_pool.register_plugin
+class NextEventsPlugin(CMSPluginBase):
+    module = _("Events")
+    name = _("Next events")
+    render_template = "fds_events/event_list.html"
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+
+        # include the entirety of today
+        events = Event.objects.get_upcoming()
+        lawsuits = Lawsuit.upcoming.all()
+
+        objects = sorted([*events, *lawsuits], key=sort_event)
+        # objects = lawsuits
+
+        context.update({"events": objects})
+        return context
---
+++ fragdenstaat_de/fds_events/models.py
@@ -0,0 +1,66 @@
+from functools import cached_property
+
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import pgettext_lazy
+
+from cms.models.fields import PlaceholderRelationField
+from cms.utils.placeholder import get_placeholder_from_slot
+
+
+class EventManager(models.Manager):
+    def get_upcoming(self):
+        tomorrow = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        return self.get_queryset().filter(end_date__gt=tomorrow).order_by("start_date")
+
+
+class Event(models.Model):
+    title = models.CharField(_("Title"), max_length=50)
+    slug = models.SlugField(_("Slug"), unique=True)
+    description = models.CharField(
+        _("Description"),
+        help_text=_("Short description, will be shown in the list view"),
+        max_length=255,
+    )
+    start_date = models.DateTimeField(_("Start"))
+    end_date = models.DateTimeField(_("End"))
+    location = models.CharField(
+        _("Location"),
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    placeholders = PlaceholderRelationField()
+
+    objects = EventManager()
+
+    class Meta:
+        verbose_name = pgettext_lazy("physical event", "Event")
+        verbose_name_plural = pgettext_lazy("physical event", "Events")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("fds_events:event-detail", kwargs={"slug": self.slug})
+
+    def get_absolute_domain_url(self):
+        return "%s%s" % (settings.SITE_URL, self.get_absolute_url())
+
+    def get_ical_url(self):
+        return reverse("fds_events:calendar-event-detail", kwargs={"slug": self.slug})
+
+    @cached_property
+    def content_placeholder(self):
+        return get_placeholder_from_slot(self.placeholders, "content")
+
+    @property
+    def is_event(self):
+        # hack to differentiate from lawsuit
+        return True
+
+    def get_template(self):
+        return "fds_events/placeholder.html"
