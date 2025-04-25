30b9b07632fca72651dff231ebe0e25d25a038ce
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-04-25 10:35:19 +0200

    Guard pixel processing task against missing pixel log file
    Could be run on a different machine

--- fragdenstaat_de/fds_mailing/tasks.py
+++ fragdenstaat_de/fds_mailing/tasks.py
@@ -1,7 +1,13 @@
+import logging
+from pathlib import Path
+
+from django.conf import settings
 from django.core.mail import mail_admins
 from django.utils import timezone
 
 from froide.celery import app as celery_app
+
+logger = logging.getLogger(__name__)
 
 
 @celery_app.task(name="fragdenstaat_de.fds_mailing.send_mailing")
@@ -51,6 +57,18 @@ def continue_sending(mailing_id):
 @celery_app.task(name="fragdenstaat_de.fds_mailing.process_pixel_log")
 def process_pixel_log():
     from .pixel_log_parsing import PixelProcessor, get_pixel_log_generator
+
+    pixel_log_path_str = settings.NEWSLETTER_PIXEL_LOG
+    if not pixel_log_path_str:
+        logger.warning("No pixel log path empty, skipping pixel log processing.")
+        return
+    pixel_log_path = Path(pixel_log_path_str)
+    if not pixel_log_path.exists():
+        logger.warning(
+            "Pixel log path %s does not exist, skipping pixel log processing.",
+            pixel_log_path,
+        )
+        return
 
     pixel_generator = get_pixel_log_generator()
     processor = PixelProcessor(pixel_generator)
