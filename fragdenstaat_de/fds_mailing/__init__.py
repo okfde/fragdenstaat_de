fc6b9009dad7d9c8c3ebf25927c30e924ece4fef
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-05-06 12:14:03 +0200

    Add preview context form to email template editor

--- fragdenstaat_de/fds_mailing/__init__.py
+++ fragdenstaat_de/fds_mailing/__init__.py
@@ -1,5 +1,15 @@
+from typing import Any, Callable, NamedTuple
+
 from django.dispatch import Signal
+from django.http import HttpRequest
 
 default_app_config = "fragdenstaat_de.fds_mailing.apps.FdsMailingConfig"
 
 mailing_submitted = Signal()  # args: []
+gather_mailing_preview_context = Signal()
+
+
+class MailingPreviewContextProvider(NamedTuple):
+    name: str
+    options: dict[str, str]
+    provide_context: Callable[[str, HttpRequest], dict[str, Any]]
