625ac97c1a05275d3370e5ab1a9f5e9e58cc59bb
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-05-14 15:12:56 +0200

    Add presence of mailing tag in query params as nl subscriber indicator

--- fragdenstaat_de/fds_newsletter/cms_plugins.py
+++ fragdenstaat_de/fds_newsletter/cms_plugins.py
@@ -55,15 +55,41 @@ class NewsletterLogicMixin:
         return context
 
     def add_to_context(self, context):
+        """
+
+        Show newsletter?
+
+        is subscriber |	not a mailing	| mailing
+            yes		  |	    n				n
+            no 		  |     y				y
+            unknown   |     y				n
+
+        """
+        mailing_link = False
+        if context.get("request"):
+            request = context["request"]
+            campaign_tag = request.GET.get("pk_campaign")
+            if campaign_tag and campaign_tag.startswith("mailing"):
+                mailing_link = True
         if not context.get("user") and context.get("request"):
             if context["request"].user.is_authenticated:
                 context["user"] = context["request"].user
+
+        is_subscriber = None  # unknown by default
         if context.get("user") and context["user"].is_authenticated:
             subscribers = Subscriber.objects.filter(
                 newsletter__slug=settings.DEFAULT_NEWSLETTER, user=context["user"]
             )
             if subscribers:
                 context["subscriber"] = subscribers[0]
+                is_subscriber = True
+            else:
+                # Definitely not a subscriber
+                is_subscriber = False
+
+        context["show_newsletter"] = is_subscriber is False or (
+            not mailing_link and is_subscriber is None
+        )
         return context
 
     def should_render(self):
@@ -75,7 +101,7 @@ class IsNewsletterSubscriberPlugin(NewsletterLogicMixin, CMSPluginBase):
     name = _("Is newsletter subscriber")
 
     def should_render(self, context):
-        return context.get("subscriber")
+        return not context["show_newsletter"]
 
 
 @plugin_pool.register_plugin
@@ -83,4 +109,4 @@ class IsNotNewsletterSubscriberPlugin(NewsletterLogicMixin, CMSPluginBase):
     name = _("Is not newsletter subscriber")
 
     def should_render(self, context):
-        return not context.get("subscriber")
+        return context["show_newsletter"]
