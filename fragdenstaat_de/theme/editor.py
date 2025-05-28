2f1664eaa86e1eba59af82aeb740db33e273c63c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-05-28 11:21:31 +0200

    Fix paths in CKEditor config

--- fragdenstaat_de/theme/editor.py
+++ fragdenstaat_de/theme/editor.py
@@ -1,15 +1,3 @@
-from cms.utils.urlutils import static_with_version
-from djangocms_text.editors import RTEConfig
+from djangocms_text.contrib.text_ckeditor4 import ckeditor4
 
-ckeditor4 = RTEConfig(
-    name="ckeditor4",
-    config="CKEDITOR",
-    js=(
-        static_with_version("cms/js/dist/bundle.admin.base.min.js"),
-        "djangocms_text/vendor/ckeditor4/ckeditor.js",
-        "djangocms_text/bundles/bundle.ckeditor4.min.js",
-    ),
-    css={"all": ("djangocms_text/css/cms.ckeditor4.css",)},
-    inline_editing=False,
-    child_plugin_support=True,
-)
+ckeditor4.inline_editing = False
