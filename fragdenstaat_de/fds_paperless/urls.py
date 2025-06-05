5dc108fd4740203db83adfc7f00d68acbe2a08e2
krmax44 <hi@krmax44.de>
2025-06-05 14:41:22 +0200

    ✨ re-create paperless import flow yet again

--- fragdenstaat_de/fds_paperless/urls.py
+++ fragdenstaat_de/fds_paperless/urls.py
@@ -1,24 +1,19 @@
 from django.urls import path
-from django.utils.translation import gettext_lazy as _
 
 from .views import (
-    SelectRequestView,
-    add_postal_message,
+    get_pdf_view,
     get_thumbnail_view,
     list_view,
+    select_documents_view,
 )
 
 urlpatterns = [
     path("", list_view, name="paperless_list"),
     path(
-        "import/<slug:slug>/",
-        add_postal_message,
+        "import/<int:foirequest>/",
+        select_documents_view,
         name="paperless_import",
     ),
-    path(
-        _("select-request/"),
-        SelectRequestView.as_view(),
-        name="paperless_select_request",
-    ),
     path("thumb/<int:paperless_document>/", get_thumbnail_view, name="paperless_thumb"),
+    path("pdf/<int:paperless_document>/", get_pdf_view, name="paperless_pdf"),
 ]
