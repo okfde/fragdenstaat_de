7d29a1126e328d351264549ca033d0eeab708ffc
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-06-27 23:13:52 +0200

    Monkey-patch page URLs of non-current sites to include domain

--- fragdenstaat_de/fds_cms/apps.py
+++ fragdenstaat_de/fds_cms/apps.py
@@ -23,6 +23,24 @@ class FdsCmsConfig(AppConfig):
         thumbnail_created.disconnect(thumbnail_created_callback)
         thumbnail_created.connect(async_optimize_thumbnail)
 
+        # Monkey-patch Page.get_absolute_url to include site domain
+
+        from django.contrib.sites.models import Site
+
+        from cms.models import Page
+
+        original_absolute_url = Page.get_absolute_url
+
+        def page_absolute_url(self, language=None, fallback=True):
+            url = original_absolute_url(self, language=language, fallback=fallback)
+            current_site = Site.objects.get_current()
+
+            if self.node.site_id != current_site.id:
+                return f"//{self.node.site.domain}{url}"
+            return url
+
+        Page.get_absolute_url = page_absolute_url
+
 
 def merge_user(sender, old_user=None, new_user=None, **kwargs):
     from .models import FoiRequestListCMSPlugin
