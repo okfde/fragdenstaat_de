130bfb8604ed4c4e0a324b3cd3f93f32670275da
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-07-17 10:32:40 +0200

    Add mail intent middleware that sends mails via ContinuousMailing
    If there's a continuous mailing with a matching email template for the mail intent,
    the mail is sent as a MailingMessage on that ContinuousMailing.

--- fragdenstaat_de/fds_mailing/middleware.py
+++ fragdenstaat_de/fds_mailing/middleware.py
@@ -1,6 +1,13 @@
+from froide.helper.email_sending import EmailContent
+
 from fragdenstaat_de.fds_newsletter.utils import has_newsletter
 
-from .models import EmailTemplate
+from .models import (
+    ContinuousMailing,
+    EmailTemplate,
+    MailingMessage,
+    MailingMessageReference,
+)
 
 
 class EmailTemplateMiddleware:
@@ -30,3 +37,34 @@ class EmailTemplateMiddleware:
             context["has_newsletter"] = has_newsletter(user)
 
         return context
+
+    def send_mail(
+        self,
+        mail_intent: str,
+        email_content: EmailContent,
+        email_address,
+        context: dict,
+        email_kwargs: dict,
+    ):
+        try:
+            mailing = ContinuousMailing.objects.get(
+                email_template__active=True,
+                email_template__mail_intent=mail_intent,
+                ready=True,
+            )
+        except ContinuousMailing.DoesNotExist:
+            return
+
+        message = MailingMessage.objects.create(
+            mailing=mailing,
+            email=email_address,
+            user=context.get("user"),
+            name=context.get("name", ""),
+            is_continuous=True,
+        )
+        if context.get("user"):
+            MailingMessageReference.objects.create_with_object(message, context["user"])
+
+        # this generates email content a second time but with a different context
+        # FIXME: don't generate email content twice
+        return message.send_message(mailing_context=context)
