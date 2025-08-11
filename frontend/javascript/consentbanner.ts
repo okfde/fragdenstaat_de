7a50d7f50aaaf1af80ce9f3482d3828ae72acb7b
krmax44 <hi@krmax44.de>
2025-08-11 12:58:32 +0200

    ✨ add consent banner for video embeds

--- frontend/javascript/consentbanner.ts
+++ frontend/javascript/consentbanner.ts
@@ -1,43 +1,101 @@
 import { Modal } from 'bootstrap'
-import {showCookieBar} from 'django-cookie-consent';
 
-
-const cookieConsentModalTemplateId = "cookie-consent__cookie-bar"
-
-const startConsentCheck = (statusUrl: string) => {
-    let modal: Modal | null = null
-    showCookieBar({
-        statusUrl,
-        templateSelector: `#${cookieConsentModalTemplateId}`,
-        cookieGroupsSelector: '#cookie-consent__cookie-groups',
-        acceptSelector: "#cookie-consent__accept",
-        declineSelector: "#cookie-consent__decline",
-        onShow: () => {
-            modal = new Modal("#cookie-consent__modal")
-            modal.show()
-        },
-        onAccept: () => {
-            modal?.hide()
-            let pixelUrls: string[] = []
-            pixelUrls = JSON.parse(document.getElementById('cookie-consent__pixelurls')?.textContent || '[]');
-            pixelUrls.forEach((url: string) => {
-                const img = document.createElement('img')
-                img.src = url
-                img.height = 1
-                img.width = 1
-                img.style.display = 'none'
-                document.body.appendChild(img)
-            })
-        },
-        onDecline: () => {
-            modal?.hide()
-        },
-    });
+const DEFAULT_FETCH_HEADERS = {
+  'X-Cookie-Consent-Fetch': '1'
 }
 
+document
+  .querySelectorAll<HTMLElement>('.cookie-consent')
+  .forEach(async (container) => {
+    const consentMessage = container
+      .querySelector<HTMLTemplateElement>('.cookie-consent__message')!
+      .content.cloneNode(true) as Element
 
+    let modal: Modal | undefined
 
-const cookieBarTemplate = document.getElementById(cookieConsentModalTemplateId)
-if (cookieBarTemplate !== null) {
-    startConsentCheck(cookieBarTemplate.dataset.cookiestatus!)
-}
+    const accepted = () => {
+      modal?.hide()
+
+      const { content } = container.querySelector<HTMLTemplateElement>(
+        '.cookie-consent__content'
+      )!
+
+      content
+        .querySelectorAll<HTMLElement>('[data-consent-src]')
+        .forEach((el) => {
+          el.setAttribute('src', el.dataset.consentSrc!)
+          el.removeAttribute('data-consent-src')
+        })
+
+      console.log(content)
+
+      container.replaceChildren()
+      container.appendChild(content.cloneNode(true))
+    }
+
+    const declined = () => {
+      modal?.hide()
+
+      const declinedMessage = container
+        .querySelector<HTMLTemplateElement>('.cookie-consent__declined')
+        ?.content.cloneNode(true)
+
+      if (declinedMessage) {
+        container.replaceChildren()
+        container.appendChild(declinedMessage)
+      } else {
+        container.remove()
+      }
+    }
+
+    const group = container.dataset.cookieGroup!
+
+    const status = await fetch(container.dataset.statusUrl!, {
+      credentials: 'same-origin',
+      headers: DEFAULT_FETCH_HEADERS
+    }).then((r) => r.json())
+
+    const postConsentStatus = async (url: string) => {
+      await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: DEFAULT_FETCH_HEADERS
+      })
+    }
+
+    if (status.acceptedCookieGroups.includes(group)) {
+      accepted()
+    } else if (!status.declinedCookieGroups.includes(group)) {
+      let buttonsContainer
+
+      if (consentMessage.firstElementChild?.classList.contains('modal')) {
+        // can't append the entire consentMessage, as that's a document fragment,
+        // and appendChild returns the copied (now empty!) fragment when appending a document fragment
+        const modalEl = document.body.appendChild(
+          consentMessage.firstElementChild
+        )
+        modal = new Modal(modalEl)
+        modal.show()
+        buttonsContainer = modalEl
+      } else {
+        container.appendChild(consentMessage)
+        buttonsContainer = container
+      }
+
+      buttonsContainer
+        .querySelector('.cookie-consent__accept')
+        ?.addEventListener('click', async () => {
+          accepted()
+          await postConsentStatus(container.dataset.acceptUrl!)
+        })
+
+      buttonsContainer
+        .querySelector('.cookie-consent__decline')
+        ?.addEventListener('click', async () => {
+          declined()
+          await postConsentStatus(container.dataset.declineUrl!)
+        })
+    } else {
+      declined()
+    }
+  })
