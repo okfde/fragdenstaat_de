e68dc87c32895b4116a36805111fc103215444f3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-08-19 20:19:17 +0100

    Make swagger-ui work with no unsafe-inline

---
+++ fragdenstaat_de/static/drf_spectacular/swagger-ui.js
@@ -0,0 +1,138 @@
+"use strict";
+
+const swaggerSettings = JSON.parse(document.getElementById('settings').textContent);
+const schemaAuthNames = JSON.parse(document.getElementById('schema_auth_names').textContent);
+const csrfHeaderName = document.currentScript.getAttribute('data-csrfheadername');
+const csrfToken = document.currentScript.getAttribute('data-csrftoken');
+const schemaUrl = document.currentScript.getAttribute('data-schemaurl');
+
+const oAuthConfigElement = document.getElementById('oauth2_config')
+let oauth2Config = null;
+if (oAuthConfigElement) {
+  oauth2Config = JSON.parse(oAuthConfigElement.textContent);
+}
+
+
+let schemaAuthFailed = false;
+const plugins = [];
+
+const reloadSchemaOnAuthChange = () => {
+  return {
+    statePlugins: {
+      auth: {
+        wrapActions: {
+          authorizeOauth2:(ori) => (...args) => {
+            schemaAuthFailed = false;
+            setTimeout(() => ui.specActions.download());
+            return ori(...args);
+          },
+          authorize: (ori) => (...args) => {
+            schemaAuthFailed = false;
+            setTimeout(() => ui.specActions.download());
+            return ori(...args);
+          },
+          logout: (ori) => (...args) => {
+            schemaAuthFailed = false;
+            setTimeout(() => ui.specActions.download());
+            return ori(...args);
+          },
+        },
+      },
+    },
+  };
+};
+
+if (schemaAuthNames.length > 0) {
+  plugins.push(reloadSchemaOnAuthChange);
+}
+
+const uiInitialized = () => {
+  try {
+    return ui !== null
+  } catch {
+    return false;
+  }
+};
+
+const isSchemaUrl = (url) => {
+  if (!uiInitialized()) {
+    return false;
+  }
+  return url === new URL(ui.getConfigs().url, document.baseURI).href;
+};
+
+const responseInterceptor = (response, ...args) => {
+  if (!response.ok && isSchemaUrl(response.url)) {
+    console.warn("schema request received '" + response.status + "'. disabling credentials for schema till logout.");
+    if (!schemaAuthFailed) {
+      // only retry once to prevent endless loop.
+      schemaAuthFailed = true;
+      setTimeout(() => ui.specActions.download());
+    }
+  }
+  return response;
+};
+
+const injectAuthCredentials = (request) => {
+  let authorized;
+  if (uiInitialized()) {
+    const state = ui.getState().get("auth").get("authorized");
+    if (state !== undefined && Object.keys(state.toJS()).length !== 0) {
+      authorized = state.toJS();
+    }
+  } else if (![undefined, "{}"].includes(localStorage.authorized)) {
+    authorized = JSON.parse(localStorage.authorized);
+  }
+  if (authorized === undefined) {
+    return;
+  }
+  for (const authName of schemaAuthNames) {
+    const authDef = authorized[authName];
+    if (authDef === undefined || authDef.schema === undefined) {
+      continue;
+    }
+    if (authDef.schema.type === "http" && authDef.schema.scheme === "bearer") {
+      request.headers["Authorization"] = "Bearer " + authDef.value;
+      return;
+    } else if (authDef.schema.type === "http" && authDef.schema.scheme === "basic") {
+      request.headers["Authorization"] = "Basic " + btoa(authDef.value.username + ":" + authDef.value.password);
+      return;
+    } else if (authDef.schema.type === "apiKey" && authDef.schema.in === "header") {
+      request.headers[authDef.schema.name] = authDef.value;
+      return;
+    } else if (authDef.schema.type === "oauth2" && authDef.token.token_type === "Bearer") {
+      request.headers["Authorization"] = `Bearer ${authDef.token.access_token}`;
+      return;
+    }
+  }
+};
+
+const requestInterceptor = (request, ...args) => {
+  if (request.loadSpec && schemaAuthNames.length > 0 && !schemaAuthFailed) {
+    try {
+      injectAuthCredentials(request);
+    } catch (e) {
+      console.error("schema auth injection failed with error: ", e);
+    }
+  }
+  // selectively omit adding headers to mitigate CORS issues.
+  if (!["GET", undefined].includes(request.method) && request.credentials === "same-origin") {
+    request.headers[csrfHeaderName] = csrfToken;
+  }
+  return request;
+};
+
+const ui = SwaggerUIBundle({
+  url: schemaUrl,
+  dom_id: "#swagger-ui",
+  presets: [SwaggerUIBundle.presets.apis],
+  plugins,
+  layout: "BaseLayout",
+  requestInterceptor,
+  responseInterceptor,
+  ...swaggerSettings,
+});
+
+if (oauth2Config) {
+  ui.initOAuth(oauth2Config);
+}
