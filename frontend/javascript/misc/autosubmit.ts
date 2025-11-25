d013d677887ef0344caf17bfd630b67ed935acf4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-11-25 14:11:43 +0100

    Add autosubmit to newsletter subscribe form

---
+++ frontend/javascript/misc/autosubmit.ts
@@ -0,0 +1,6 @@
+const autoSubmitForm = document.querySelector("form[data-autosubmit]") as HTMLFormElement | null
+if (autoSubmitForm && autoSubmitForm.checkValidity()) {
+    // Use click on type submit instead of calling .submit()
+    // so 'submit' event handler runs
+    autoSubmitForm.querySelector<HTMLElement>("[type='submit']")?.click()
+}
