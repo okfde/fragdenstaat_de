d259ac15b027c68878e72f40cdc47cab83bdf30c
Anett Seeker <noeddl@users.noreply.github.com>
2026-01-06 11:35:04 +0100

    ✅ Restructure tests

--- fragdenstaat_de/theme/tests/conftest.py
+++ fragdenstaat_de/theme/tests/conftest.py
@@ -1,11 +1,8 @@
 import re
 
-from django.conf import settings
-
 import pytest
-from django_elasticsearch_dsl.registries import registry
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Document, Index, Text
+from elasticsearch_dsl import Document, Text
 
 from froide.helper.search import (
     SearchQuerySetWrapper,
@@ -19,81 +16,55 @@ from froide.helper.search.filters import BaseSearchFilterSet
 from fragdenstaat_de.fds_blog.models import Article
 from fragdenstaat_de.theme.tests.testdata.search_docs import search_docs
 
-ES_CONFIG = settings.ELASTICSEARCH_DSL["default"]
+
+class TestDocument(Document):
+    content = Text(
+        analyzer=get_text_analyzer(),
+        search_analyzer=get_search_analyzer(),
+        search_quote_analyzer=get_search_quote_analyzer(),
+        index_options="offsets",
+    )
+
+    class Django:
+        model = Article
 
 
 @pytest.fixture(scope="session")
 def elasticsearch_client():
-    return Elasticsearch(hosts=ES_CONFIG["hosts"])
+    return Elasticsearch("http://localhost:9200")
 
 
 @pytest.fixture(scope="session")
 def test_index(elasticsearch_client):
-    index_name = "test_index"
-    index = Index(index_name, using=elasticsearch_client)
+    # Set up the test index.
+    index = get_index("docs")
+    index.document(TestDocument)
+    # Timeout has to be set here - is ignored when set in Elasticsearch().
+    index.create(timeout="60s")
 
-    index.analyzer(get_text_analyzer())
-    index.analyzer(get_search_analyzer())
-    index.analyzer(get_search_quote_analyzer())
+    # Create test documents.
+    for doc in search_docs:
+        doc_id = doc.get("id")
+        TestDocument(**doc).save(id=doc_id)
 
-    index.create()
+    TestDocument._index.refresh()
 
-    yield index_name
+    yield index
 
+    # Clean up after the test run.
     index.delete()
 
 
 @pytest.fixture(scope="session")
-def analyze(elasticsearch_client, test_index):
+def analyze(test_index):
     def _analyze(analyzer_name, text):
-        result = elasticsearch_client.indices.analyze(
-            index=test_index, body={"analyzer": analyzer_name, "text": text}
+        result = test_index.analyze(
+            body={"analyzer": analyzer_name, "text": text},
         )
 
         return [t["token"] for t in result["tokens"]]
 
     return _analyze
-
-
-@pytest.fixture(scope="session")
-def test_document_class(elasticsearch_client):
-    index = get_index("test_index")
-    analyzer = get_text_analyzer()
-    search_analyzer = get_search_analyzer()
-    search_quote_analyzer = get_search_quote_analyzer()
-
-    @registry.register_document
-    @index.document
-    class TestDocument(Document):
-        content = Text(
-            analyzer=analyzer,
-            search_analyzer=search_analyzer,
-            search_quote_analyzer=search_quote_analyzer,
-            index_options="offsets",
-        )
-
-        class Django:
-            model = Article
-
-    index.create()
-
-    yield TestDocument
-
-    index.delete()
-
-
-@pytest.fixture(scope="session")
-def create_test_documents(test_document_class):
-    docs = []
-    for doc in search_docs:
-        doc_id = doc.get("id")
-        doc_obj = test_document_class(**doc)
-        doc_obj.save(id=doc_id)
-        docs.append(doc_obj)
-
-    test_document_class._index.refresh()
-
-    return docs
 
 
 @pytest.fixture(scope="session")
@@ -105,7 +76,7 @@ def test_search_filterset():
 
 
 @pytest.fixture(scope="session")
-def search(test_document_class, create_test_documents, test_search_filterset):
+def search(test_index, test_search_filterset):
     def _search(query):
         data = {}
         if query:
@@ -117,7 +88,7 @@ def search(test_document_class, create_test_documents, test_search_filterset):
             {"_default_manager": type("Manager", (), {"all": lambda: None})},
         )
 
-        search = test_document_class.search()
+        search = test_index.search()
         search = search.highlight_options(encoder="html").highlight("content")
 
         sqs = SearchQuerySetWrapper(search, mock_model)
