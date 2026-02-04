bae042c662867189e7d89ba80f2fcb6c34ada596
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-02-04 20:13:31 +0100

    Add test for CMS config

---
+++ tests/test_cms_config.py
@@ -0,0 +1,26 @@
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize("configuration", ["Gegenrechtsschutz", "Ubf"])
+@pytest.mark.django_db
+def test_cms_configuration(configuration):
+    # Need to run subprocess to properly run Django's
+    # import machinery
+    completed_process = subprocess.run(
+        [
+            "python",
+            "manage.py",
+            "check",
+            "--settings",
+            "fragdenstaat_de.settings.development",
+            "--configuration",
+            configuration,
+        ],
+        capture_output=True,
+        text=True,
+    )
+    assert (
+        completed_process.returncode == 0
+    ), f"Configuration {configuration} failed check: {completed_process.stdout} {completed_process.stderr}"
