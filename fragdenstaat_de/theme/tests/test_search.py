2c02f5e42ada574adb21590f5221fd8c72a0cb06
Anett Seeker <noeddl@users.noreply.github.com>
2026-02-04 14:30:47 +0100

    ✅ Add test for correct definitions of analyzers for search

--- fragdenstaat_de/theme/tests/test_search.py
+++ fragdenstaat_de/theme/tests/test_search.py
@@ -1,4 +1,12 @@
 import pytest
+from django_elasticsearch_dsl.fields import TextField
+
+from froide.helper.search import (
+    get_ngram_analyzer,
+    get_search_analyzer,
+    get_search_quote_analyzer,
+    get_text_analyzer,
+)
 
 from fragdenstaat_de.theme.search import QueryPreprocessor, get_decompounder_analyzer
 from fragdenstaat_de.theme.tests.testdata.search_queries import (
@@ -11,6 +19,91 @@ from fragdenstaat_de.theme.tests.testdata.search_tokens import (
     search_quote_analyzer_tests,
     text_analyzer_tests,
 )
+
+ANALYZERS = {
+    "analyzer": get_text_analyzer(),
+    "search_analyzer": get_search_analyzer(),
+    "search_quote_analyzer": get_search_quote_analyzer(),
+}
+
+
+def get_document_classes():
+    from froide_evidencecollection.documents import EvidenceDocument, PersonDocument
+
+    from froide.foirequest.documents import FoiRequestDocument
+    from froide.publicbody.documents import PublicBodyDocument
+
+    from fragdenstaat_de.fds_blog.documents import ArticleDocument
+    from fragdenstaat_de.fds_cms.documents import CMSDocument
+
+    return [
+        ArticleDocument,
+        CMSDocument,
+        FoiRequestDocument,
+        PublicBodyDocument,
+        EvidenceDocument,
+        PersonDocument,
+    ]
+
+
+def get_text_fields(document_class):
+    return {
+        field_name: field
+        for field_name, field in document_class._fields.items()
+        if isinstance(field, TextField)
+    }
+
+
+class TestDocumentAnalyzerConfiguration:
+    # Text fields that are not intended for full-text search.
+    NON_FTS_FIELDS = ["author", "first_name", "last_name", "url"]
+
+    @pytest.mark.parametrize(
+        "document_class", get_document_classes(), ids=lambda doc: doc.__name__
+    )
+    def test_text_fields_have_correct_analyzers(self, document_class):
+        """Test that all text fields in the document have the correct analyzers configured."""
+        text_fields = get_text_fields(document_class)
+
+        for field_name, field in text_fields.items():
+            if field_name == "name_auto":
+                self._assert_ngram_field(field_name, field)
+            elif field_name in self.NON_FTS_FIELDS:
+                self._assert_no_analyzers(field_name, field)
+            else:
+                self._assert_full_text_analyzers(field_name, field)
+
+    def _assert_ngram_field(self, field_name, field):
+        """Test that the field is configured to use the ngram analyzer."""
+        assert (
+            field.analyzer == get_ngram_analyzer()
+        ), f"Field '{field_name}' should use ngram analyzer"
+        # If no separate search analyzer is set, field.analyzer is used for searching.
+        assert (
+            hasattr(field, "search_analyzer") is False
+        ), f"Field '{field_name}' should not have 'search_analyzer' set"
+        assert (
+            hasattr(field, "search_quote_analyzer") is False
+        ), f"Field '{field_name}' should not have 'search_quote_analyzer' set"
+
+    def _assert_no_analyzers(self, field_name, field):
+        """Test that the field has no analyzers set, i.e. uses the standard analyzer."""
+        for analyzer_type in ANALYZERS.keys():
+            analyzer = getattr(field, analyzer_type, None)
+            assert (
+                analyzer is None
+            ), f"Field '{field_name}' should not have '{analyzer_type}' set"
+
+    def _assert_full_text_analyzers(self, field_name, field):
+        """Test that the field has the correct full-text analyzers set."""
+        for analyzer_type, expected_analyzer in ANALYZERS.items():
+            actual_analyzer = getattr(field, analyzer_type, None)
+            actual_name = actual_analyzer._name if actual_analyzer else None
+
+            assert actual_name == expected_analyzer._name, (
+                f"Field '{field_name}' has incorrect {analyzer_type}: "
+                f"{actual_name} != {expected_analyzer._name}"
+            )
 
 
 @pytest.mark.elasticsearch
