8c2a6fca2eaa06bac6c2305b4b05a49c6ff4c24a
krmax44 <hi@krmax44.de>
2026-02-05 11:58:52 +0100

    ✅ test production in cms config test

--- tests/test_cms_config.py
+++ tests/test_cms_config.py
@@ -1,4 +1,6 @@
+import os
 import subprocess
+from pathlib import Path
 
 import pytest
 
@@ -8,18 +10,21 @@ import pytest
 def test_cms_configuration(configuration):
     # Need to run subprocess to properly run Django's
     # import machinery
+    env = {**os.environ, "DJANGO_MEDIA_ROOT": str(Path("./media").resolve())}
+
     completed_process = subprocess.run(
         [
             "python",
             "manage.py",
             "check",
             "--settings",
-            "fragdenstaat_de.settings.development",
+            "fragdenstaat_de.settings.production",
             "--configuration",
             configuration,
         ],
         capture_output=True,
         text=True,
+        env=env,
     )
     assert (
         completed_process.returncode == 0
