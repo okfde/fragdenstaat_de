aa2289f0b34b841e46c82bf86a12f0e2a4a990de
krmax44 <hi@krmax44.de>
2026-02-09 11:47:16 +0100

    ⚰️ remove ask function

--- devsetup.sh
+++ devsetup.sh
@@ -25,42 +25,6 @@ if [[ $(basename "$PWD") == "$MAIN" ]]; then
   cd ..
 fi
 
-
-ask() {
-  # https://djm.me/ask
-  local prompt default reply
-
-  if [ "${2:-}" = "Y" ]; then
-    prompt="Y/n"
-    default=Y
-  elif [ "${2:-}" = "N" ]; then
-    prompt="y/N"
-    default=N
-  else
-    prompt="y/n"
-    default=
-  fi
-
-  while true; do
-    # Ask the question (not using "read -p" as it uses stderr not stdout)
-    echo -n "$1 [$prompt] "
-
-    # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-    read reply </dev/tty
-
-    # Default?
-    if [ -z "$reply" ]; then
-      reply=$default
-    fi
-
-    # Check if the reply is valid
-    case "$reply" in
-      Y*|y*) return 0 ;;
-      N*|n*) return 1 ;;
-    esac
-  done
-}
-
 check_versions() {
   echo "You need python, uv and pnpm 9 installed."
 
