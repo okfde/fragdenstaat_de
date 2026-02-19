e33939200af0c0f2d8d82c4ed322843cbd09fe75
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-02-19 14:20:21 +0100

    Fix copy value by click for nullboolean select in merge donor UI

--- fragdenstaat_de/fds_donation/static/js/merge_donors.js
+++ fragdenstaat_de/fds_donation/static/js/merge_donors.js
@@ -1,15 +1,27 @@
-(function(){
-
-  function applyValue (valueElement) {
+;(function () {
+  function applyValue(valueElement) {
     var fieldName = valueElement.dataset.field
     var value = valueElement.dataset.value
     var el = document.querySelector(`[name="${fieldName}"]`)
-    el.value = value
+    if (el.tagName === 'SELECT') {
+      if (value === '' && el.querySelector("option[value='']")) {
+        el.value = ''
+      } else if (value === '') {
+        el.value = 'unknown'
+      } else if (
+        el.querySelector("option[value='" + value.toLowerCase() + "']")
+      ) {
+        el.value = value.toLowerCase()
+      } else {
+        el.value = value
+      }
+    } else {
+      el.value = value
+    }
   }
-  
-  document.querySelectorAll('[data-donor]').forEach(p => {
+
+  document.querySelectorAll('[data-donor]').forEach((p) => {
     p.style.cursor = 'copy'
     p.addEventListener('click', () => applyValue(p))
   })
-  
-  }())
+})()
