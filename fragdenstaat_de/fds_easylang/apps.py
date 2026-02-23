ca83d81aa28defcbd41a0626e0606404fa6296f1
Anett Seeker <noeddl@users.noreply.github.com>
2026-02-23 15:26:14 +0100

    ✨ Add fds_easylang app

---
+++ fragdenstaat_de/fds_easylang/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class FdsEasylangConfig(AppConfig):
+    name = "fragdenstaat_de.fds_easylang"
+    verbose_name = _("FragDenStaat Easy Language App")
