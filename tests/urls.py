218771b46d096bce321c26449b2a055b040c4b0d
Anett Seeker <noeddl@users.noreply.github.com>
2026-02-24 13:02:48 +0100

    ✅ Make test URLs match production setup more closely

--- tests/urls.py
+++ tests/urls.py
@@ -1,19 +1,26 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 
 from fragdenstaat_de.theme.urls import urlpatterns as base_urlpatterns
 
 urlpatterns = [
-    path("blog/", include("fragdenstaat_de.fds_blog.urls", namespace="blog")),
     path("cms/search/", include("fragdenstaat_de.fds_cms.urls", namespace="fds_cms")),
     path(
         "cms/contact/",
         include("fragdenstaat_de.fds_cms.contact", namespace="fds_cms_contact"),
     ),
+    path("crowdfunding/", include("froide_crowdfunding.urls")),
+    path("food/", include("froide_food.urls")),
+    path("exam/", include("froide_exam.urls")),
+]
+
+urlpatterns += i18n_patterns(
+    path("blog/", include("fragdenstaat_de.fds_blog.urls", namespace="blog")),
     path(
         "spenden/spende/",
         include("fragdenstaat_de.fds_donation.urls", namespace="fds_donation"),
     ),
-    path("crowdfunding/", include("froide_crowdfunding.urls")),
-    path("food/", include("froide_food.urls")),
-    path("exam/", include("froide_exam.urls")),
-] + base_urlpatterns
+    prefix_default_language=False,
+)
+
+urlpatterns += base_urlpatterns
