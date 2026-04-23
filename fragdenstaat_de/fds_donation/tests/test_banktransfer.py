2381a0e122bffe66410433d29c90e7723e95ab26
krmax44 <hi@krmax44.de>
2026-04-23 14:56:43 +0200

    ♻️ use async playwright

--- fragdenstaat_de/fds_donation/tests/test_banktransfer.py
+++ fragdenstaat_de/fds_donation/tests/test_banktransfer.py
@@ -3,7 +3,7 @@ import re
 from django.urls import reverse
 
 import pytest
-from playwright.sync_api import Page
+from playwright.async_api import Page
 
 from fragdenstaat_de.fds_donation.models import Donation
 
@@ -18,32 +18,36 @@ def banktransfer_setup(settings, live_server):
     settings.ALLOWED_HOSTS = ["*"]
 
 
-def fill_donation_page(page: Page, donor_email):
-    page.get_by_placeholder("Vorname").fill("Peter")
-    page.get_by_placeholder("Nachname").fill("Parker")
-    page.get_by_placeholder("z.B. name@beispiel.de").fill(donor_email)
-    page.get_by_text("Nein, danke.").nth(1).click()
-    page.get_by_text("Nein, danke.").nth(2).click()
-    page.get_by_label("Was ist drei plus vier?").fill("7")
+@pytest.mark.asyncio(loop_scope="session")
+async def fill_donation_page(page: Page, donor_email):
+    await page.get_by_placeholder("Vorname").fill("Peter")
+    await page.get_by_placeholder("Nachname").fill("Parker")
+    await page.get_by_placeholder("z.B. name@beispiel.de").fill(donor_email)
+    await page.get_by_text("Nein, danke.").nth(1).click()
+    await page.get_by_text("Nein, danke.").nth(2).click()
+    await page.get_by_label("Was ist drei plus vier?").fill("7")
 
 
 DONATION_DONE_URL = re.compile(r".*spenden/spende/spenden/abgeschlossen/.*")
 
 
+@pytest.mark.asyncio(loop_scope="session")
 @pytest.mark.django_db
-def test_banktransfer_once(page: Page, live_server, banktransfer_setup, mailoutbox):
+async def test_banktransfer_once(
+    page: Page, live_server, banktransfer_setup, mailoutbox
+):
     donor_email = "peter.parker@example.com"
 
-    page.goto(live_server.url + reverse("fds_donation:donate"))
-    page.get_by_role("button", name="5 Euro").click()
-    page.get_by_text("Überweisung", exact=True).click()
-    fill_donation_page(page, donor_email)
+    await page.goto(live_server.url + reverse("fds_donation:donate"))
+    await page.get_by_role("button", name="5 Euro").click()
+    await page.get_by_text("Überweisung", exact=True).click()
+    await fill_donation_page(page, donor_email)
 
-    page.get_by_role("button", name="Jetzt spenden").click()
-    page.wait_for_url(DONATION_DONE_URL)
+    await page.get_by_role("button", name="Jetzt spenden").click()
+    await page.wait_for_url(DONATION_DONE_URL)
 
-    assert page.get_by_text("Vielen Dank für Deine Spende!").is_visible()
-    assert page.get_by_text(donor_email).is_visible()
+    assert await page.get_by_text("Vielen Dank für Deine Spende!").is_visible()
+    assert await page.get_by_text(donor_email).is_visible()
 
     donation = Donation.objects.filter(
         donor__email=donor_email, amount=5, recurring=False, method="banktransfer"
@@ -57,23 +61,24 @@ def test_banktransfer_once(page: Page, live_server, banktransfer_setup, mailoutb
     assert "IBAN" in message.body
 
 
+@pytest.mark.asyncio(loop_scope="session")
 @pytest.mark.django_db
-def test_banktransfer_recurring(
+async def test_banktransfer_recurring(
     page: Page, live_server, banktransfer_setup, mailoutbox
 ):
     donor_email = "peter.parker@example.com"
 
-    page.goto(live_server.url + reverse("fds_donation:donate"))
-    page.get_by_role("button", name="5 Euro").click()
-    page.get_by_text("monatlich").click()
-    page.get_by_text("Überweisung", exact=True).click()
-    fill_donation_page(page, donor_email)
+    await page.goto(live_server.url + reverse("fds_donation:donate"))
+    await page.get_by_role("button", name="5 Euro").click()
+    await page.get_by_text("monatlich").click()
+    await page.get_by_text("Überweisung", exact=True).click()
+    await fill_donation_page(page, donor_email)
 
-    page.get_by_role("button", name="Jetzt spenden").click()
-    page.wait_for_url(DONATION_DONE_URL)
+    await page.get_by_role("button", name="Jetzt spenden").click()
+    await page.wait_for_url(DONATION_DONE_URL)
 
-    assert page.get_by_text("Vielen Dank für Deine Spende!").is_visible()
-    assert page.get_by_text(donor_email).is_visible()
+    assert await page.get_by_text("Vielen Dank für Deine Spende!").is_visible()
+    assert await page.get_by_text(donor_email).is_visible()
 
     donation = Donation.objects.filter(
         donor__email=donor_email, amount=5, recurring=True, method="banktransfer"
