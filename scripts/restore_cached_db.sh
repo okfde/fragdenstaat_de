f18e42925a16925f83618802ef6a661bf668b225
krmax44 <hi@krmax44.de>
2026-04-27 17:34:25 +0200

    👷 cache database migrations in ci

---
+++ scripts/restore_cached_db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# restore a cached database dump for use with pytest-xdist,
+# which uses a seperate database with each thread
+
+set -e
+
+CPUS=$(nproc --all)
+RESTORE="fds_restore"
+DB_USER="fragdenstaat_de"
+
+gunzip dump.gz
+
+docker compose -f compose-dev.yaml exec db createdb -U $DB_USER -O $DB_USER $RESTORE
+docker compose -f compose-dev.yaml exec -T db psql -U $DB_USER -X $RESTORE < dump
+
+for i in $(seq 0 $(($CPUS - 1))); do
+    DB_NAME="test_${DB_USER}_gw${i}"
+    echo $DB_NAME
+    docker compose -f compose-dev.yaml exec db psql -U $DB_USER -c "CREATE DATABASE $DB_NAME WITH TEMPLATE $RESTORE OWNER $DB_USER;"
+done
