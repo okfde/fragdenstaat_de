51e2c0ac7a285d8c082cded36e33a4cbe573df20
Anett Seeker <noeddl@users.noreply.github.com>
2026-04-29 13:43:49 +0200

    🐛 Add fallback for EASYLANG_ENABLED in context processor

--- fragdenstaat_de/theme/context_processors.py
+++ fragdenstaat_de/theme/context_processors.py
@@ -5,5 +5,5 @@ def theme_settings(request):
     return {
         "SENTRY_JS_URL": settings.SENTRY_JS_URL,
         "MATOMO_SITE_ID": settings.MATOMO_SITE_ID,
-        "EASYLANG_ENABLED": settings.EASYLANG_ENABLED,
+        "EASYLANG_ENABLED": getattr(settings, "EASYLANG_ENABLED", False),
     }
