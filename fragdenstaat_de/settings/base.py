5ba1162858d2fc83ea6081d6b4ac137719caa32b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-05-04 16:06:39 +0200

    Add @bmwe.de to recipient block list

--- fragdenstaat_de/settings/base.py
+++ fragdenstaat_de/settings/base.py
@@ -801,7 +801,8 @@ class FragDenStaatBase(German, Base):
                     r"^pad\.donotreply@frontex\.europa\.eu|"
                     r"^noreply@.*|^empfangsbestaetigung@bahn\.de$|.*\.local$|^postmaster@.*|"
                     r"^askema\.noreply@ema\.europa\.eu$|^.*@nomail\.ec\.europa\.eu$|"
-                    r"^eingangsbestaetigung@jobcenter-ge\.de$"
+                    r"^eingangsbestaetigung@jobcenter-ge\.de$|"
+                    r"@bmwe\.de$",
                 ),
                 "content_urls": {
                     "terms": "/nutzungsbedingungen/",
